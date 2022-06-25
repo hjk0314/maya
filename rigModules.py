0b043698c90a4e02d22f178d2160aa19bc4b87f7
hjk0314 <hjk0314@gmail.com>
2022-06-26 01:31:48 +0900

    add Functions. about create locator at the midPoints.

--- rigModules.py
+++ rigModules.py
@@ -16,3 +16,27 @@ def createCurveUsingLocator(closedCurve): # input : True or False
             circleName = cmds.circle(c=(0, 0, 0), nr=(0, 1, 0), sw=360, r=1, d=3, ch=False, s=len(sel))[0]
             for j, k in enumerate(locatorPosition):
                 cmds.move(k[0], k[1], k[2], '%s.cv[%d]' % (circleName, j), ws=True)
+
+
+def getMiddlePoint(sel): # sel : tuple in list
+    allPoints = [cmds.xform(i, q=True, t=True, ws=True) for i in sel]
+    try:
+        middlePoints = [(allPoints[0][i] + allPoints[1][i]) / 2 for i in range(3)] # x, y, z coordinates
+        return middlePoints
+    except:
+        return False
+
+
+def createLocatorMidpoint():
+    sel = cmds.ls(sl=True, fl=True)
+    selNumber = len(sel)
+    if selNumber == 1: # Select One point.
+        position = cmds.xform(sel[0], q=True, t=True, ws=True)
+    elif selNumber == 2: # Select Two points.
+        position = getMiddlePoint(sel)
+    else:
+        position = (0, 0, 0)
+    locator = cmds.spaceLocator()
+    cmds.xform(locator, t=position, ws=True)
+
+
