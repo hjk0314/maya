7de45ca07f3a24eff60cb2828b4a38eed0d24930
hjk0314 <hjk0314@gmail.com>
2022-09-03 21:20:50 +0900

    connect DB pattern

--- hjk.py
+++ hjk.py
@@ -4,6 +4,10 @@ import json
 import os
 import codecs
 import itertools
+import pandas as pd
+import pyodbc
+import urllib
+import sqlalchemy
 
 
 # Export to json file and shading networks. And assign to them.
@@ -704,4 +708,56 @@ def columnToNumber(column):
                     yield count
                     break
                 count += 1
+
+
+# using urlib
+# using sqlalchemy
+# using pandas
+def usingSqlalchemy():
+    conn_str = (
+        r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};'
+        r'DBQ=../folder/file.accdb;'
+    )
+    conn_url = f"access+pyodbc:///?odbc_connect={urllib.parse.quote_plus(conn_str)}"
+    acc_engine = sqlalchemy.create_engine(conn_url)
+    csv_path = "../folder/file.csv"
+    csv_data = pd.read_csv(csv_path)
+    csv_data.to_sql('table', acc_engine, if_exists='append', index=False)
+
+
+# using pyodbc only
+def usingPyodbc():
+    conn_str = (
+        r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};'
+        r'DBQ=../folder/file.accdb;'
+    )
+    conn = pyodbc.connect(conn_str)
+    cursor = conn.cursor()
+    # insert
+    sql = r"Insert into tabel (uName, wDate) Values ('jkhng', '2022-09-03')"
+    cursor.execute(sql)
+    cursor.commit()
+    # select all
+    sql = r"select * from table"
+    cursor.excute(sql)
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
+    conn.close()
+
+
+# using pandas
+# using pyodbc
+def usingPandas():
+    conn_str = (
+        r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};'
+        r'DBQ=../folder/file.accdb;'
+    )
+    conn = pyodbc.connect(conn_str)
+    sql = r"Select * From table"
+    df = pd.read_spl(sql, conn)
+    wDate  = [str(i) for i in df['wDate'] if str(i).startswith('2022-05')]
+    print(wDate)
+    print(df.head(3))
+    conn.close()
 
--- temp3.py
+++ temp3.py
@@ -4,62 +4,54 @@ import urllib
 from sqlalchemy import create_engine
 
 
-conn_str = (
-    r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};'
-    r'DBQ=C:\Users\jkhong\Desktop\crewPlan.accdb;'
-)
-# conn = pyodbc.connect(conn_str)
-# cursor = conn.cursor()
-# cursor.execute('select * from 2022')
-query = 'select * from 2022'
-conn_url = f"access+pyodbc:///?odbc_connect={urllib.parse.quote_plus(conn_str)}"
-acc_engine = create_engine(conn_url)
-
-csv_path = r"W:\SP팀\크루플랜\2022\ART_Team_민청식_data.csv"
-csv_data = pd.read_csv(csv_path)
-
-csv_data.to_sql('2022', acc_engine, if_exists='append', index=False)
-print('done')
+# using urlib
+# using sqlalchemy
+# using pandas
+def usingSqlalchemy():
+    conn_str = (
+        r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};'
+        r'DBQ=../folder/file.accdb;'
+    )
+    conn_url = f"access+pyodbc:///?odbc_connect={urllib.parse.quote_plus(conn_str)}"
+    acc_engine = create_engine(conn_url)
+    csv_path = "../folder/file.csv"
+    csv_data = pd.read_csv(csv_path)
+    csv_data.to_sql('table', acc_engine, if_exists='append', index=False)
 
 
-# df = pd.read_sql(query, conn)
-# df.to_sql('2022', cursor, if_exists='append')
-# print(df.head())
-
-# for row in cursor.fetchall():
-#     print (row)
-
-
-
-
-# conn = (
-#     r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};'
-#     r'DBQ=C:\Users\jkhong\Desktop\crewPlan.accdb;'
-# )
-# print([i for i in pyodbc.drivers() if i.startswith('MS Access Driver')])
-# cursor = conn.cursor()
-# cursor.excute('select * from 2022')
-
-# for i in cursor.fetchall():
-#     print(i)
-
+# using pyodbc only
+def usingPyodbc():
+    conn_str = (
+        r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};'
+        r'DBQ=../folder/file.accdb;'
+    )
+    conn = pyodbc.connect(conn_str)
+    cursor = conn.cursor()
+    # insert
+    sql = r"Insert into tabel (uName, wDate) Values ('jkhng', '2022-09-03')"
+    cursor.execute(sql)
+    cursor.commit()
+    # select all
+    sql = r"select * from table"
+    cursor.excute(sql)
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
+    conn.close()
 
 
-# csv_path = "C:/Users/jkhong/Desktop/ANI_Team_홍진기_data.csv"
-# data = pd.read_csv(csv_path)
-# # # print(data.head(10))
-# # # print(data['날짜'])
-# # wDate  = [str(i) for i in data['날짜'] if str(i).startswith('2022-05')]
-# # print(wDate)
+# using pandas
+# using pyodbc
+def usingPandas():
+    conn_str = (
+        r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};'
+        r'DBQ=../folder/file.accdb;'
+    )
+    conn = pyodbc.connect(conn_str)
+    sql = r"Select * From table"
+    df = pd.read_spl(sql, conn)
+    wDate  = [str(i) for i in df['wDate'] if str(i).startswith('2022-05')]
+    print(wDate)
+    print(df.head(3))
+    conn.close()
 
-
-# # pd.DataFrame({"날짜": classes})
-# cnn_str = (
-#     r"Driver={Microsoft Access Driver (*.mdb, *.accdb)};"
-#     r"DBQ=C:/Users/jkhong/Desktop/crewPlan.accdb"
-# )
-
-# cnn_url = f"access+pyodbc:///?odbc_connect={urllib.parse.quote_plus(cnn_str)}"
-# acc_engine = create_engine(cnn_url)
-# data.to_sql('2022', acc_engine, if_exists='append')
-# print("write complete.")
