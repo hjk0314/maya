a61b2df8eb2ea4f56f82355d41e14edfd6810f71
hjk0314 <hjk0314@gmail.com>
2022-09-04 23:41:15 +0900

    temp4.py update

--- temp4.py
+++ temp4.py
@@ -1,5 +1,9 @@
 import os
 import codecs
+import pandas as pd
+import pyodbc
+import urllib
+from sqlalchemy import create_engine
 
 
 def saveAs():
@@ -17,3 +21,31 @@ def saveAs():
         else:
             pass
 
+
+# using urlib
+# using sqlalchemy
+# using pandas
+def usingSqlalchemy():
+    conn_str = (
+        r"Driver={Microsoft Access Driver (*.mdb, *.accdb)};"
+        r"DBQ=C:\Users\hjk03\Desktop\crewPlan\crewPlanDB.accdb;"
+    )
+    conn_url = f"access+pyodbc:///?odbc_connect={urllib.parse.quote_plus(conn_str)}"
+    acc_engine = create_engine(conn_url)
+    csv_folder = "C:/Users/hjk03/Desktop/crewPlan/csv/new"
+    csv_files = os.listdir(csv_folder)
+    for i in csv_files:
+        csv_file = csv_folder + '/' + i
+        if os.path.isfile(csv_file):
+            try:
+                df = pd.read_csv(csv_file)
+                print(df.head(1))
+                df.to_sql('2022', acc_engine, if_exists='append', index=True)
+            except:
+                print('this')
+                continue
+        else:
+            pass
+
+
+usingSqlalchemy()
