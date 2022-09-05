a9b2bb653c6cb1bffed7d9971c75db1f310018fb
hjk0314 <hjk0314@gmail.com>
2022-09-05 18:55:33 +0900

    editing for mmp

--- temp4.py
+++ temp4.py
@@ -7,8 +7,8 @@ from sqlalchemy import create_engine
 
 
 def saveAs():
-    csv_folder = "C:/Users/hjk03/Desktop/crewPlan/csv"
-    csv_newFolder = "C:/Users/hjk03/Desktop/crewPlan/csv/new"
+    csv_folder = "W:/SP팀/크루플랜/2022"
+    csv_newFolder = "C:/Users/jkhong/Desktop/크루플랜_테스트"
     csv_files = os.listdir(csv_folder)
     for i in csv_files:
         csv_old = csv_folder + '/' + i
@@ -16,8 +16,11 @@ def saveAs():
             csv_new = csv_newFolder + '/' + i
             with open(csv_old, 'r') as txtOld:
                 lines = txtOld.readlines()
-            with codecs.open(csv_new, 'w', 'utf-8') as txtNew:
-                txtNew.writelines(lines)
+            if lines:
+                with codecs.open(csv_new, 'w', 'utf-8-sig') as txtNew:
+                    txtNew.writelines(lines)
+            else:
+                print(f"No data : {i}")
         else:
             pass
 
@@ -28,24 +31,21 @@ def saveAs():
 def usingSqlalchemy():
     conn_str = (
         r"Driver={Microsoft Access Driver (*.mdb, *.accdb)};"
-        r"DBQ=C:\Users\hjk03\Desktop\crewPlan\crewPlanDB.accdb;"
+        r"DBQ=C:\Users\jkhong\Desktop\크루플랜_테스트\crewPlan.accdb;"
     )
     conn_url = f"access+pyodbc:///?odbc_connect={urllib.parse.quote_plus(conn_str)}"
     acc_engine = create_engine(conn_url)
-    csv_folder = "C:/Users/hjk03/Desktop/crewPlan/csv/new"
+    csv_folder = "C:/Users/jkhong/Desktop/크루플랜_테스트/csv_copied"
     csv_files = os.listdir(csv_folder)
     for i in csv_files:
         csv_file = csv_folder + '/' + i
         if os.path.isfile(csv_file):
             try:
                 df = pd.read_csv(csv_file)
-                print(df.head(1))
                 df.to_sql('2022', acc_engine, if_exists='append', index=True)
             except:
-                print('this')
+                print(f'Error : {i}')
                 continue
         else:
             pass
 
-
-usingSqlalchemy()
