5dba9286b65c79f7166b821a730b333656c04978
hjk0314 <hjk0314@gmail.com>
2022-09-16 19:10:26 +0900

    autowheel almost done

--- test.py
+++ test.py
@@ -13,86 +13,130 @@ class AutoWheel():
     def setupUI(self):
         if pm.window('Auto_Wheel', exists=True):
             pm.deleteUI('Auto_Wheel')
-        titleStr = 'Automatically_rotate_the_wheel'
-        win = pm.window('Auto_Wheel', t=titleStr, s=True, rtf=True)
+        ttl = 'Automatically_rotate_the_wheel'
+        win = pm.window('Auto_Wheel', t=ttl, s=True, rtf=True)
         pm.columnLayout(cat=('both', 4), rs=2, cw=295)
-        sel = pm.ls(sl=True)
-        dic = {}
-        for j, k in enumerate(sel):
+        self.dic = {}
+        for i in range(4):
+            wheelStr = 'wheel_%d' % i
+            ctrlStr = 'ctrl_%d' % i
             pm.separator(h=10)
             pm.rowColumnLayout(nc=2, cw=[(1, 60), (2, 226)])
-            pm.text('wheel_%s :  ' % j, al='right')
-            obj = pm.textField(ed=False, tx=k)
-            pm.text('ctrl_%s :  ' % j, al='right')
-            cuv = pm.textField(ed=True, tx=f'cc_{k}')
+            pm.text(f'{wheelStr} :  ', al='right')
+            wheelField = pm.textField(ed=False, pht='wheel\'s name')
+            pm.text(f'{ctrlStr} :  ', al='right')
+            ctrlField = pm.textField(ed=True, pht='controller\'s name')
             pm.setParent("..", u=True)
-            dic[obj] = cuv
+            self.dic[wheelField] = ctrlField
         pm.separator(h=10)
-        bgColor = (0.269, 0.509, 0.617)
-        # bgColor = (0.839, 0.422, 0.176)
-        msg1 = 'Change the radius later in the controller.'
-        msg2 = 'Nothing selected.'
-        msg = msg1 if sel else msg2
-        self.conditions = pm.textField(ed=False, tx=msg, bgc=bgColor)
+        self.conditions = pm.textField(ed=False)
         pm.separator(h=10)
-        if sel:
-            lbl = 'Create'
-            fn = 'lambda x: self.main(dic)'
-        else:
-            lbl = 'Select the wheel first'
-            fn = 'lambda x: self.setupUI()'
-        pm.button(l=lbl, c=exec(fn))
+        pm.button(l='Fill the blanks', c=lambda x: self.fill())
+        pm.button(l='Create', c=lambda x: self.main())
         pm.separator(h=10)
         pm.showWindow(win)
 
-# 79 char line ================================================================
-# 72 docstring or comments line ========================================
 
     def fill(self):
         sel = pm.ls(sl=True)
-        if not sel:
-            self.conditions.setText('Nothing selected.')
-        elif pm.ls(sel, dag=True, type=['nurbsCurve']):
-            print('goto curve')
-        elif pm.ls(sel, dag=True, type=['mesh']):
-            objList = [i.name() for i in sel]
-            objStr = ', '.join(objList)
-            radList = self.getRadius(objList)
-            radStr = [str(i) for i in radList]
-            radStr = ', '.join(radStr)
-            self.ctrlName.setText(objStr)
-            self.wheelRadius.setText(radStr)
-        else:
-            self.conditions.setText('Unknown objects.')
+        num = len(sel)
+        for j, k in enumerate(self.dic):
+            if j < num:
+                obj = sel[j]
+                k.setText(obj)
+                self.dic[k].setText(f'cc_{obj}')
+            else:
+                break
 
 
-    def getRadius(self, obj: list) -> list:
-        result = []
-        for i in obj:
-            bbWheel = pm.xform(i, q=True, bb=True)
-            xMin, yMin, zMin, xMax, yMax, zMax = bbWheel
-            x = (xMax - xMin) / 2
-            y = (yMax - yMin) / 2
-            z = (zMax - zMin) / 2
-            bbList = [x, y, z]
-            bbList.sort(reverse=True)
-            bb = bbList[0] # biggest
-            bb = round(bb, 3) # bb: float
-            result.append(bb)
+    def main(self):
+        # cc: Curve Controller
+        ccDic = self.check()
+        print(ccDic)
+        # for i in ccDic:
+            # ccDic = {cuvName: wheelRadius, ...}
+            # self.createCC(i, ccDic[i])
+
+
+    def check(self):
+        result = {}
+        for i in self.dic:
+            wheel = i.getText()
+            curve = self.dic[i].getText()
+            colon = ':' in curve
+            dup = '|' in curve
+            rad = self.getRadius(wheel)
+            if not wheel: # is or not
+                msg = 'Nothing selected.'
+                bgColor = (0.839, 0.422, 0.176)
+                break
+            elif not curve: # is or not
+                msg = 'The Curve Controller Name field is empty.'
+                bgColor = (0.839, 0.422, 0.176)
+                break
+            elif not rad: # zero or not
+                msg = 'The radius must be non-zero.'
+                bgColor = (0.839, 0.422, 0.176)
+                break
+            elif colon: # is colon included or not
+                msg = 'The controllers name contains a colon.'
+                bgColor = (0.839, 0.422, 0.176)
+                break
+            elif dup: # is duplicated or not
+                msg = 'You should check for duplicate names.'
+                bgColor = (0.839, 0.422, 0.176)
+                break
+            else:
+                msg = 'Successfully added it to the dictionary.'
+                bgColor = (0.269, 0.509, 0.617)
+                result[curve] = rad
+        self.conditions.setText(msg)
+        self.conditions.setBackgroundColor(bgColor)
         return result
 
 
-    def main(self, a):
-        # a = self.ctrlName.getText()
-        print(a)
+    def getRadius(self, obj: str) -> float:
+        bbWheel = pm.xform(obj, q=True, bb=True)
+        xMin, yMin, zMin, xMax, yMax, zMax = bbWheel
+        x = (xMax - xMin) / 2
+        y = (yMax - yMin) / 2
+        z = (zMax - zMin) / 2
+        bbList = [x, y, z]
+        bbList.sort(reverse=True)
+        bb = bbList[0] # biggest
+        result = round(bb, 3)
+        return result
 
 
-    def createCC(self, obj: str, rad: float) -> None:
-        cuv = 'cc_' + obj
+    # Create curve Controllers, joints, and groups.
+    def createCC(self, cuv: str, rad: float) -> None:
+        # Variables
+        jnt, nullGrp, prevGrp, orntGrp, exprStr = self.createVar(cuv)
+        # Create in maya
+        pm.circle(n=cuv, nr=(1,0,0), r=rad, ch=False)
+        pm.addAttr(cuv, ln='Radius', at='double', dv=1)
+        pm.setAttr(f'{cuv}.Radius', e=True, k=True)
+        pm.setAttr(f'{cuv}.Radius', rad)
+        pm.addAttr(cuv, ln='AutoRoll', at='long', min=0, max=1, dv=1)
+        pm.setAttr(f'{cuv}.AutoRoll', e=True, k=True)
+        for i in ['X', 'Y', 'Z']:
+            pm.addAttr(cuv, ln=f'PrevPos{i}', at='double', dv=0)
+            pm.setAttr(f'{cuv}.PrevPos{i}', e=True, k=True)
+        pm.joint(n=jnt, p=(0,0,0))
+        pm.group(n=nullGrp, em=True, p=cuv)
+        pm.group(n=prevGrp, em=True, w=True)
+        pm.group(n=orntGrp, em=True, p=prevGrp)
+        pm.aimConstraint(cuv, prevGrp, mo=False)
+        pm.orientConstraint(nullGrp, orntGrp, mo=False)
+        pm.expression(s=exprStr, o='', ae=1, uc='all')
+
+
+    # Return variables.
+    def createVar(self, cuv: str) -> tuple:
         jnt = cuv + '_jnt'
         nullGrp = cuv + '_nullGrp'
-        prevGrp = obj + '_prevGrp'
-        orntGrp = obj + '_orntGrp'
+        prevGrp = cuv + '_prevGrp'
+        orntGrp = cuv + '_orntGrp'
         # expression1 ==================================================
         expr1 = f'float $R = {cuv}.Radius;'
         expr1 += f'float $A = {cuv}.AutoRoll;'
@@ -125,25 +169,8 @@ class AutoWheel():
         # Final expression string
         exprStr = expr1 + expr2 + expr3 + expr4
         # expression end ===============================================
-
-        cuv = pm.circle(n=cuv, nr=(1,0,0), r=rad, ch=False)
-        cuv = cuv[0]
-        pm.addAttr(cuv, ln='Radius', at='double', dv=1)
-        pm.setAttr(f'{cuv}.Radius', e=True, k=True)
-        pm.addAttr(cuv, ln='AutoRoll', at='long', min=0, max=1, dv=1)
-        pm.setAttr(f'{cuv}.AutoRoll', e=True, k=True)
-        for i in ['X', 'Y', 'Z']:
-            pm.addAttr(cuv, ln=f'PrevPos{i}', at='double', dv=0)
-            pm.setAttr(f'{cuv}.PrevPos{i}', e=True, k=True)
-        pm.joint(n=jnt, p=(0,0,0))
-        nullGrp = pm.group(n=nullGrp, em=True, p=cuv)
-        # nextGrp = pm.group(n=nextGrp, em=True, w=True)
-        prevGrp = pm.group(n=prevGrp, em=True, w=True)
-        orntGrp = pm.group(n=orntGrp, em=True, p=prevGrp)
-        # pm.connectAttr(f'{cuv}.translate', f'{nextGrp}.translate', f=True)
-        pm.aimConstraint(cuv, prevGrp, mo=False)
-        pm.orientConstraint(nullGrp, orntGrp, mo=False)
-        pm.expression(s=exprStr, o='', ae=1, uc='all')
+        result = (jnt, nullGrp, prevGrp, orntGrp, exprStr)
+        return result
 
 
 AutoWheel()
