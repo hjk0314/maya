7af8c23be36dd60d23bba420377cc47a0b5d5b0e
hjk0314 <hjk0314@gmail.com>
2022-10-25 19:22:57 +0900

    first upload - class PenetratingCurve()

--- test.py
+++ test.py
@@ -1,54 +1,10 @@
-import maya.cmds as cmd
-import maya.mel as mel
-import re
+import pymel.core as pm
 
 
-def makeCenterLine(c=1, j=1):
-    points = 'curve -d 3 '
-    sel = cmd.ls(os=True, fl = True)
-    ob = cmd.ls(o=True, sl=True)
-    num = []
-    out = []
-    edges = []
-    joints = []
-    for one in sel:
-        strip = re.search(r"\[([0-9]+)\]", one)
-        num.append(strip.group(1))
-    size = len(num)
-    if (size):
-        if size == 1:
-            edges = cmd.polySelect(edgeRing=(int(num[0])), ns = True)
-        if size == 2:
-            edges = cmd.polySelect(edgeRingPath=(int(num[0]), int(num[1])), ns = True)
-        if size > 2:
-            edges = num
-        for one in edges:
-            cmd.select(ob)
-            cmd.polySelect(elb=int(one))
-            clust = cmd.cluster(n = 'poo#')
-            cmd.select(clear=True)
-            posi = cmd.getAttr(clust[0]+'HandleShape.origin')
-            if c:
-                points = points + ('-p %s %s %s ' % (posi[0][0], posi[0][1], posi[0][2]))
-            if j:
-                joints.append(cmd.joint(p = (posi[0][0], posi[0][1], posi[0][2])))
-            cmd.delete (clust[0])
-        if c:
-            out.append(mel.eval(points))
-        if j:
-            for i in reversed(range(1, len(edges))):
-                cmd.parent(joints[i], joints[i-1])
-            cmd.joint(joints[0], e=True, oj = 'xyz', sao= 'zup', ch=True, zso=True)
-            cmd.joint(joints[-1],e=True, o =(0,0,0))
-            out.append(joints[0])
-        cmd.select(out)
-    else:
-        print("Nothing is selected")
-
-
-makeCenterLine(1, 0)
-
-
-# 79 char line ================================================================
-# 72 docstring or comments line ========================================
-
+sel = pm.ls(sl=True, dag=True)
+channel = [".tx", ".ty", ".tz", ".rx", ".ry", ".rz", ".sx", ".sy", ".sz", ".v"]
+jnt = [i for i in sel if pm.objectType(i, i='joint')]
+for i in jnt:
+    pm.delete(i, cn=True)
+    for j in channel:
+        pm.disconnectAttr(i + j)
