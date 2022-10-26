87cade1c4665a6343fc2291b42686dba08911284
hjk0314 <hjk0314@gmail.com>
2022-10-26 19:22:09 +0900

    usual update

--- test.py
+++ test.py
@@ -1,10 +1,55 @@
+import re
+import itertools
 import pymel.core as pm
 
 
-sel = pm.ls(sl=True, dag=True)
-channel = [".tx", ".ty", ".tz", ".rx", ".ry", ".rz", ".sx", ".sy", ".sz", ".v"]
-jnt = [i for i in sel if pm.objectType(i, i='joint')]
-for i in jnt:
-    pm.delete(i, cn=True)
-    for j in channel:
-        pm.disconnectAttr(i + j)
+def channelList():
+    TRS = ['t', 'r', 's']
+    XYZ = ['x', 'y', 'z']
+    PRO = itertools.product(TRS, XYZ)
+    # PRO: [('t', 'x'), ('t', 'y'), ... ('s', 'z')]
+    channel = [''.join(i) for i in PRO]
+    channel += ['v']
+    # ['tx', 'ty', 'tz', 'rx', 'ry', 'rz', 'sx', 'sy', 'sz', 'v']
+    return channel
+
+
+def selConst():
+    sel = pm.ls(sl=True, dag=True)
+    STR = re.compile(r'.+Constraint[0-9]?')
+    constList = []
+    for i in sel:
+        typ = pm.objectType(i)
+        if STR.search(typ):
+            constList.append(i)
+        else:
+            continue
+    # pm.select(constList)
+    return constList
+
+
+def selJnt():
+    sel = pm.ls(sl=True, dag=True)
+    jnt = []
+    for i in sel:
+        typ = pm.objectType(i, i='joint')
+        if typ:
+            jnt.append(i)
+        else:
+            continue
+    # pm.select(jnt)
+    return jnt
+
+
+def main():
+    const = selConst()
+    jnt = selJnt()
+    channel = channelList()
+    pm.delete(const)
+    for i in jnt:
+        pm.delete(i, cn=True)
+        for j in channel:
+            pm.disconnectAttr(i + j)
+
+
+main()
