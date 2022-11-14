9e84d556dbc437df184c345d0f8ead2c70575ce3
hjk0314 <hjk0314@gmail.com>
2022-11-14 23:02:25 +0900

    test3.py almost done.

--- test3.py
+++ test3.py
@@ -5,50 +5,88 @@ import pymel.core as pm
 # Select mesh only.
 def selObj() -> list:
     sel = pm.ls(sl=True, s=True, dag=True)
-    meshList = {i.getParent() for i in sel if pm.objectType(i) == "mesh"}
-    result = list(meshList)
-    # pm.select(result)
+    meshList = []
+    for i in sel:
+        if pm.objectType(i) == "mesh":
+            obj = i.getParent()
+            meshList.append(obj)
+        else:
+            continue
+    meshSet = set(meshList)
+    result = list(meshSet)
     return result
 
 
-sel = selObj()
-selShp = pm.ls(sel, dag=True, s=True)
-referencedList = {i for i in selShp if pm.referenceQuery(i, inr=True)}
-referencedList = list(referencedList)
-deformedList = []
-for i in selShp:
-    A = ":" in i.name()
-    B = "Deformed" in i.name()
-    if not A and B:
-        deformedList.append(i)
+def getReferenced(shape: list) -> list:
+    result = {i for i in shape if pm.referenceQuery(i, inr=True)}
+    result = list(result)
+    return result
 
-syncDict = {}
-for i in deformedList:
-    temp = re.search('(.*)Deformed', i.name())
-    obj = temp.group(1)
-    for j in referencedList:
-        ref = j.rsplit(":", 1)[-1]
-        if obj == ref:
-            syncDict[j] = i
+
+def getDeformed(shape: list) -> list:
+    result = []
+    for i in shape:
+        A = ":" in i.name()
+        B = "Deformed" in i.name()
+        if not A and B:
+            result.append(i)
+    return result
+
+
+def getCompared(ref: list, rig: list) -> dict:
+    result = {}
+    for i in rig:
+        temp = re.search('(.*)Deformed', i.name())
+        obj = temp.group(1)
+        for j in ref:
+            ref = j.rsplit(":", 1)[-1]
+            if obj == ref:
+                result[j] = i
+            else:
+                continue
+    return result
+
+
+def makeTheSame(compare: dict) -> str:
+    for ref, rig in compare.items():
+        A = pm.listAttr(ref, r=True, sa=True, lf=True)
+        B = pm.listAttr(rig, r=True, sa=True, lf=True)
+        attrA = {}
+        for i in A:
+            try:
+                attrA[i] = pm.getAttr(f'{ref}.{i}')
+            except:
+                continue
+        attrB = {}
+        for i in B:
+            try:
+                attrB[i] = pm.getAttr(f'{rig}.{i}')
+            except:
+                continue
+        diff = [i for i in attrA if attrA[i] != attrB[i]]
+        diff.remove('intermediateObject')
+        if not diff:
+            msg = 'Nothing to do.'
         else:
-            continue
-# print(syncDict)
-for j, k in syncDict.items():
-    attrA = pm.listAttr(j, r=True, sa=True, lf=True)
-    attrB = pm.listAttr(k, r=True, sa=True, lf=True)
-    attrA_Dict = {}
-    for i in attrA:
-        try:
-            attrA_Dict[i] = pm.getAttr(f'{j}.{i}')
-        except:
-            continue
-    attrB_Dict = {}
-    for i in attrB:
-        try:
-            attrB_Dict[i] = pm.getAttr(f'{k}.{i}')
-        except:
-            continue
-diffList = [i for i in attrA_Dict if attrA_Dict[i] != attrB_Dict[i]]
-print(diffList)
-for i in diffList:
-    pm.setAttr(f"{k}.{i}", attrA_Dict[i])
+            msg = 'Matched the shape.'
+            for i in diff:
+                pm.setAttr(f"{rig}.{i}", attrA[i])
+        return msg
+
+def main():
+    sel = selObj()
+    if not sel:
+        print("Nothing selected.")
+    else:
+        shp = pm.ls(sel, dag=True, s=True)
+        ref = getReferenced(shp)
+        rig = getDeformed(shp)
+        cmp = getCompared(ref, rig)
+        msg = makeTheSame(cmp)
+        if msg == None:
+            print("There is no Deformed shape.")
+        else:
+            print(msg)
+
+
+main()
