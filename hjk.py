95d304f0e9a59817ab201ce06cf443c7c6bea9bc
hjk0314 <hjk0314@gmail.com>
2023-01-13 15:43:54 +0900

    hjk.py and test.py update

--- hjk.py
+++ hjk.py
@@ -1275,6 +1275,17 @@ def grp():
         pm.move(0,0,0, grp+".scalePivot", grp+".rotatePivot", rpr=True)
 
 
+# Grouping null
+def grpNull():
+    sel = pm.ls(sl=True)
+    for i in sel:
+        grp = pm.group(i, n=f"{i}_null", r=True, )
+        x, y, z = pm.xform(i, q=1, ws=1, rp=1)
+        sp = f"{grp}.scalePivot"
+        rp = f"{grp}.rotatePivot"
+        pm.move(x, y, z, sp, rp, rpr=True)
+
+
 # Create an empty group and match the pivot with the selector.
 def grpEmpty():
     sel = pm.ls(sl=True)
@@ -1724,73 +1735,3 @@ def hjkCopy():
 # 72 docstring or comments line ========================================
 
 
-def growingJoint():
-    sel = pm.ls(sl=True)
-    pointList = []
-    jointList = []
-    pm.select(cl=True)
-    for i in sel:
-        jntName = i.replace('loc_', 'jnt_')
-        print(jntName)
-        point = pm.xform(i, q=1, ws=1, rp=1)
-        joint = pm.joint(p=point, n=jntName, rad=50)
-        pointList.append(point)
-        jointList.append(joint)
-    pm.select(jointList[0])
-    orientJnt()
-    ikHName = jointList[0].replace('jnt_', 'ikH_')
-    pm.ikHandle(sj=jointList[0], ee=jointList[1], sol='ikSCsolver', n=ikHName)
-    Dimension = pm.distanceDimension(sp=pointList[0], ep=pointList[1])
-    Distance = pm.getAttr(f"{Dimension}.distance")
-    Distance = round(Distance, 3)
-    expr = f"{jointList[0]}.scaleX = {Dimension}.distance / {Distance};"
-    pm.expression(s=expr, o='', ae=1, uc='all')
-
-
-# growingJoint()
-# rename('spoiler_L', 'spoiler_R')
-
-
-def grpNull():
-    sel = pm.ls(sl=True)
-    for i in sel:
-        grp = pm.group(i, n=f"{i}_null", r=True, )
-        x, y, z = pm.xform(i, q=1, ws=1, rp=1)
-        sp = f"{grp}.scalePivot"
-        rp = f"{grp}.rotatePivot"
-        pm.move(x, y, z, sp, rp, rpr=True)
-
-
-# grpNull()
-# ctrl(pointer=True)
-# color(red=True)
-# color(blue=True)
-# color(blue2=True)
-# color(pink=True)
-# color(green=True)
-# color(green2=True)
-# color(red2=True)
-# color(yellow=True)
-
-
-# 79 char line ================================================================
-# 72 docstring or comments line ========================================
-
-
-def cleanFBX():
-    sel = pm.ls(sl=True)
-    obj, jnt = sel
-    fullPath = pm.Env().sceneName()
-    bName = os.path.basename(fullPath)
-    name, ext = os.path.splitext(bName)
-    pm.rename(obj, name)
-    pm.select(jnt, hi=True)
-    pm.delete(sc=True, uac=False, hi=0, cp=0, s=0)
-    rename("Bip01FBXASC032", "jnt_")
-    rename("FBXASC032", "_")
-    pm.rename(jnt, 'jnt_root')
-    pm.currentUnit(t="film")
-
-
-# a = om.MFileIO_currentFile()
-# om.MFileIO_open('C:/users/jkhong/Desktop/a.ma')
