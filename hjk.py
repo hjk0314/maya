083c31c537225123bd6cbe4f77932f5b630e6f33
hjk0314 <hjk0314@gmail.com>
2023-01-25 21:51:42 +0900

    hjk.py and test.py upload

--- hjk.py
+++ hjk.py
@@ -1840,33 +1840,56 @@ def pointPosition():
 
 def createChannels():
     sel = pm.ls(sl=True)
-    # channelList = [
-    #     "Toe", 
-    #     "Bank", 
-    #     "Twist", 
-    #     "Heel", 
-    #     "Ball", 
-    #     "Down", 
-    #     ]
     channelList = [
-        "FKIK_L_F", 
-        "FKIK_R_F", 
-        "FKIK_L_B", 
-        "FKIK_R_B", 
+        "Toe", 
+        "Bank", 
+        "Twist", 
+        "Heel", 
+        "Ball", 
+        "Down", 
         ]
+    # channelList = [
+    #     "FKIK_L_F", 
+    #     "FKIK_R_F", 
+    #     "FKIK_L_B", 
+    #     "FKIK_R_B", 
+    #     ]
     for i in sel:
         for cName in channelList:
-            pm.addAttr(i, ln=cName, at='double', min=0, max=10, dv=0)
+            pm.addAttr(i, ln=cName, at='double', dv=0)
             pm.setAttr(f'{i}.{cName}', e=True, k=True)
+
+
+def connectBlendColors():
+    sel = pm.ls(sl=True)
+    tmp = int(len(sel) / 3)
+    IK = [sel[i] for i in range(tmp)]
+    FK = [sel[i] for i in range(tmp, (tmp*2))]
+    FBX = [sel[i] for i in range((tmp*2), (tmp*3))]
+    SWITCH = "cc_global.FKIK_Tail"
+    setR = pm.shadingNode("setRange", au=True)
+    pm.connectAttr(SWITCH, f"{setR}.valueX", f=True)
+    for i in range(tmp):
+        createBlendColors(SWITCH, setR, IK[i], FK[i], FBX[i])
+
+
+def createBlendColors(SWITCH, setR, IK, FK, FBX):
+    bls = pm.shadingNode("blendColors", au=True)
+    pm.connectAttr(f"{FK}.rotate", f"{bls}.color1", f=True)
+    pm.connectAttr(f"{IK}.rotate", f"{bls}.color2", f=True)
+    pm.connectAttr(f"{bls}.output", f"{FBX}.rotate", f=True)
+    pm.setAttr(f"{setR}.oldMaxX", 10)
+    pm.setAttr(f"{setR}.maxX", 1)
+    pm.connectAttr(f"{setR}.outValueX", f"{bls}.blender", f=True)
 
 
 
 # createChannels()
-# ctrl(sph=True)
-# rename("FK", "IK")
+# ctrl(cub=True)
+# rename("clt_tailIK_1")
 # sel = pm.ls(sl=True)
 # new = sel[0]
 # old = sel[1]
 # pm.matchTransform(new, old, pos=True)
-
 # pm.rename(new, old)
+# connectBlendColors()
--- test.py
+++ test.py
@@ -1,4 +1,35 @@
 import pymel.core as pm
 import maya.mel as mel
+import re
+import os
 
+
+folder = "U:/DOJ/Rnd/Horse/Output/ABC/horseGroomV0006_sim_v0102"
+searchList = ['hair', 'add', ]
+extension = '.abc'
+fileList = os.listdir(folder)
+for i in fileList:
+    name, ext = os.path.splitext(i)
+    for j in searchList:
+        if ext != extension:
+            continue
+        elif re.match(f'.*{j}.*', i):
+            print(f"{folder}/{i}")
+            pm.importFile(f"{folder}/{i}")
+        else:
+            print(f"There are no files matching the {j}")
+
+    #     tmp.group(1)
+
+    # print(checkList)
+    # for k in checkList:
+    #     if ext == extension:
+    #     else:
+    #         continue
+
+
+# for i in sel:
+#     tmp = re.match(f'(.*){search}(.*)', i)
+
+# print(tmp)
 
