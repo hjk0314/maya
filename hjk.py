f30b8ae74af37171686b9fb392e768e0e73ac3d1
hjk0314 <hjk0314@gmail.com>
2023-02-08 11:29:48 +0900

    update hjk.py and test.py

--- hjk.py
+++ hjk.py
@@ -7,6 +7,7 @@ import maya.OpenMaya as om
 import pymel.core as pm
 import maya.mel as mel
 import sympy
+import itertools
 from math import *
 
 
@@ -1846,65 +1847,5 @@ def pointPosition():
 # 79 char line ================================================================
 # 72 docstring or comments line ========================================
 
-
-def createChannels():
-    sel = pm.ls(sl=True)
-    channelList = [
-        "Toe", 
-        "Bank", 
-        "Twist", 
-        "Heel", 
-        "Ball", 
-        "Down", 
-        ]
-    # channelList = [
-    #     "FKIK_L_F", 
-    #     "FKIK_R_F", 
-    #     "FKIK_L_B", 
-    #     "FKIK_R_B", 
-    #     ]
-    for i in sel:
-        for cName in channelList:
-            pm.addAttr(i, ln=cName, at='double', dv=0)
-            pm.setAttr(f'{i}.{cName}', e=True, k=True)
-
-
-def connectBlendColors():
-    sel = pm.ls(sl=True)
-    tmp = int(len(sel) / 3)
-    IK = [sel[i] for i in range(tmp)]
-    FK = [sel[i] for i in range(tmp, (tmp*2))]
-    FBX = [sel[i] for i in range((tmp*2), (tmp*3))]
-    SWITCH = "cc_global.FKIK_Tail"
-    setR = pm.shadingNode("setRange", au=True)
-    pm.connectAttr(SWITCH, f"{setR}.valueX", f=True)
-    for i in range(tmp):
-        createBlendColors(SWITCH, setR, IK[i], FK[i], FBX[i])
-
-
-def createBlendColors(SWITCH, setR, IK, FK, FBX):
-    bls = pm.shadingNode("blendColors", au=True)
-    pm.connectAttr(f"{FK}.rotate", f"{bls}.color1", f=True)
-    pm.connectAttr(f"{IK}.rotate", f"{bls}.color2", f=True)
-    pm.connectAttr(f"{bls}.output", f"{FBX}.rotate", f=True)
-    pm.setAttr(f"{setR}.oldMaxX", 10)
-    pm.setAttr(f"{setR}.maxX", 1)
-    pm.connectAttr(f"{setR}.outValueX", f"{bls}.blender", f=True)
-
-
-
-# createChannels()
-# ctrl(pointer=True)
-# rename("cc_tailIK_3")
-# rename("cc_spineIn_1")
-# sel = pm.ls(sl=True)
-# new = sel[0]
-# old = sel[1]
-# pm.matchTransform(new, old, pos=True)
-# pm.rename(new, old)
-# connectBlendColors()
-# sel = pm.ls(sl=True)
-# for i in sel:
-#     pm.connectAttr("multiplyDivide6.output", f"{i}.scale", f=True)
 
 
