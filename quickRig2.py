1d6f142fcfa2ea1906572221e004a9ee16cb3ca0
hjk0314 <hjk0314@gmail.com>
2023-03-20 19:20:30 +0900

    hjk.py and quickRig2.py update

--- quickRig2.py
+++ quickRig2.py
@@ -3,7 +3,7 @@ import pymel.core as pm
 
 
 class Coordinates:
-    ctrl = {
+    CTRL = {
         "cub": [
             (-1, 1, -1), (-1, 1, 1), (1, 1, 1), 
             (1, 1, -1), (-1, 1, -1), (-1, -1, -1), 
@@ -38,11 +38,31 @@ class Coordinates:
             (-6, 0, -14), (-6, 0, 18), (-6, 12, 6), 
             (6, 12, 6), (6, 0, 18), (6, 0, -14), (6, 12, -14), 
         ], 
+        "car": [
+            (81, 70, 119), (89, 56, 251), (89, -12, 251), 
+            (89, -12, 117), (89, -12, -117), (89, -12, -229), 
+            (81, 70, -229), (81, 70, -159), (69, 111, -105), 
+            (69, 111, 63), (81, 70, 119), (-81, 70, 119), 
+            (-89, 56, 251), (-89, -12, 251), (-89, -12, 117), 
+            (-89, -12, -117), (-89, -12, -229), (-81, 70, -229), 
+            (-81, 70, -159), (-69, 111, -105), (69, 111, -105), 
+            (81, 70, -159), (-81, 70, -159), (-81, 70, -229), 
+            (81, 70, -229), (89, -12, -229), (-89, -12, -229), 
+            (-89, -12, -117), (-89, -12, 117), (-89, -12, 251), 
+            (89, -12, 251), (89, 56, 251), (-89, 56, 251), 
+            (-81, 70, 119), (-69, 111, 63), (-69, 111, -105), 
+            (69, 111, -105), (69, 111, 63), (-69, 111, 63)
+        ], 
+        "car2": [
+            (165, 0, -195), (0, 0, -276), (-165, 0, -195), 
+            (-97, 0, -0), (-165, -0, 195), (-0, -0, 276), 
+            (165, -0, 195), (97, -0, 0), (165, 0, -195), 
+        ], 
     }
-
-
-    jnt = {
-        'root': {'jnt_root': (0.0, 91.146, -2.311)}, 
+    HUMAN = {
+        'root': {
+            'jnt_root': (0.0, 91.146, -2.311)
+        }, 
         'spine': {
             'jnt_spine_1': (0.0, 101.79, -0.828), 
             'jnt_spine_2': (-0.0, 112.04, -0.291), 
@@ -146,9 +166,59 @@ class Coordinates:
             'jnt_pinky_R_4': (-75.285, 123.158, -13.366), 
         }, 
     }
+    CAR = {
+        "root": {
+            "jnt_root": (0, 15, 0), 
+            "jnt_body": (0, 45, 0), 
+            "jnt_body_end": (0, 145, 0), 
+        }, 
+        "wheel_L_Ft": {
+            "jnt_wheel_L_Ft": (70, 30, 140), 
+            "jnt_wheel_L_Ft_end": (85, 30, 140), 
+        }, 
+        "wheel_R_Ft": {
+            "jnt_wheel_R_Ft": (-70, 30, 140), 
+            "jnt_wheel_R_Ft_end": (-85, 30, 140), 
+        }, 
+        "wheel_L_Bk": {
+            "jnt_wheel_L_Bk": (70, 30, -140), 
+            "jnt_wheel_L_Bk_end": (85, 30, -140), 
+        }, 
+        "wheel_R_Bk": {
+            "jnt_wheel_R_Bk": (-70, 30, -140), 
+            "jnt_wheel_R_Bk_end": (-85, 30, -140), 
+        }, 
+    }
 
 
-class Curves:
+def tempJnt() -> str:
+    all = Coordinates.jnt
+    head = [list(i.keys())[0] for i in all.values()]
+    for jntDict in all.values():
+        pm.select(cl=True)
+        for name, pos in jntDict.items():
+            jnt = pm.joint(p=(0, 0, 0), n=name, rad=10)
+            pm.move(jnt, pos)
+        jntList = list(jntDict)
+        Tools().orientJnt(jntList)
+    cuv = Tools().createCuv(50)
+    pm.parent(head, cuv)
+    return cuv
+
+
+class Tools:
+    def __init__(self):
+        pass
+
+
+    def orientJnt(self, jnt: list) -> None:
+        init = jnt[0]
+        last = jnt[-1]
+        pm.makeIdentity(jnt, a=True, jo=True, n=0)
+        pm.joint(init, e=True, oj='xyz', sao='yup', ch=True, zso=True)
+        pm.joint(last, e=True, oj='none', ch=True, zso=True)
+
+
     def createCuv(self, size: float, **kwargs) -> str:
         nor = {
             "x": (1, 0, 0), 
@@ -167,33 +237,98 @@ class Curves:
                     continue
                 else:
                     axis = nor[k]
-        cuv = pm.circle(nr=axis, ch=False)
+        cuv = pm.circle(nr=axis, ch=False)[0]
         pm.scale(cuv, [size, size, size])
         pm.makeIdentity(cuv, a=True, n=0)
         return cuv
 
 
-class Joints:
-    def orientJnt(self, jnt: list) -> None:
-        init = jnt[0]
-        last = jnt[-1]
-        pm.makeIdentity(jnt, a=True, jo=True, n=0)
-        pm.joint(init, e=True, oj='xyz', sao='yup', ch=True, zso=True)
-        pm.joint(last, e=True, oj='none', ch=True, zso=True)
-                
+    def getHeadJnt(self, *arg):
+        sel = pm.ls(arg, dag=True, type=['transform'])
+        jnt = []
+        for i in sel:
+            typ = pm.objectType(i)
+            if typ != 'joint' or i.endswith("_end"):
+                continue
+            else:
+                jnt.append(i)
+        return jnt
 
-def tempJnt() -> str:
-    all = Coordinates.jnt
-    head = [list(i.keys())[0] for i in all.values()]
-    for jntDict in all.values():
-        pm.select(cl=True)
-        for name, pos in jntDict.items():
-            jnt = pm.joint(p=(0, 0, 0), n=name, rad=10)
-            pm.move(jnt, pos)
-        jntList = list(jntDict)
-        Joints().orientJnt(jntList)
-    cuv = Curves().createCuv(50)
-    pm.parent(head, cuv)
-    return cuv
+
+class quickRig_CAR:
+    def __init__(self):
+        self.setupUI()
+        # self.createJoint()
+        # self.createCtrl()
+
+
+    def setupUI(self):
+        winName = 'QuickRig_CAR'
+        if pm.window(winName, exists=True):
+            pm.deleteUI(winName)
+        win = pm.window(winName, t='Car Rig', s=True, rtf=True)
+        pm.columnLayout(cat=('both', 4), rs=2, cw=178)
+        pm.separator(h=10)
+        self.tempCircle = pm.textField(ed=True)
+        pm.button(l="Create Joints", c=lambda x: self.createJoint())
+        pm.button(l="Create Ctrls", c=lambda x: self.createCtrl())
+        pm.button(l="Close", c=lambda x: pm.deleteUI(winName))
+        pm.separator(h=10)
+        pm.showWindow(win)
+
+
+    def createJoint(self):
+        branch = []
+        for i in Coordinates.CAR.values():
+            pm.select(cl=True)
+            for name, pos in i.items():
+                jnt = pm.joint(p=(0, 0, 0), n=name, rad=10)
+                pm.move(jnt, pos)
+            jntList = list(i.keys())
+            branch.append(jntList[0])
+            Tools().orientJnt(jntList)
+        init = branch.pop(0)
+        for j in branch:
+                pm.parent(j, init)
+        cuv = Tools().createCuv(300, y=True)
+        self.tempCircle.setText(cuv)
+        pm.parent(init, cuv)
+        
+
+    def createCtrl(self):
+        cir = self.tempCircle.getText()
+        pos = Coordinates.CTRL
+        jnt = Tools().getHeadJnt(cir)
+        ccList = []
+        for i in jnt:
+            if i == "jnt_root":
+                cc_main = pm.circle(nr=(0, 1, 0), n="cc_main", r=300, ch=0)
+                cc_main = cc_main[0]
+                cc_sub = pm.curve(d=1, p=pos["car2"], n="cc_sub")
+                ccList.append(cc_main)
+                ccList.append(cc_sub)
+            elif i == "jnt_body":
+                cc_body = pm.curve(d=1, p=pos["car"], n="cc_body")
+                ccList.append(cc_body)
+                pm.matchTransform(cc_body, i, pos=True)
+            else:
+                ccName = i.replace("jnt_", "cc_")
+                cc_etc = pm.circle(nr=(1, 0, 0), n=ccName, r=40, ch=0)
+                ccList.append(cc_etc)
+                pm.matchTransform(cc_etc, i, pos=True)
+
+
+        # cirScale = pm.getAttr(f"{cir}.scale")
+        
+        # print(cc_body, cc_sub, cc_main)
+        # pm.makeIdentity(cc_main, a=True, n=0)
+        # pm.scale(cc_body, cirScale)
+        # pm.scale(cc_sub, cirScale)
+        # pm.delete(cir)
+        
+
+
+
+    
 
 
