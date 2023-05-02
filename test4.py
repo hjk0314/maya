3d4e6fa71da7c5c6f057622e55d8246c0bc5f182
hjk0314 <hjk0314@gmail.com>
2023-05-02 19:22:55 +0900

    test4.py update

--- test4.py
+++ test4.py
@@ -52,15 +52,61 @@ def temp5():
         pm.setAttr(f"clt_{i}_grp.visibility", 0)
 
 
-jntList = [f"jnt_{i}" for i in range(3, 298, 2)]
-numList = [i for i in range(1, 149)]
-numList = sorted(numList, reverse=True)
-obj = "prop_bandolier_mdl_v9999:bandolier_magazine"
-objList = [f"{obj}_{i}_grp" for i in numList]
-print(jntList)
-print(numList)
-print(len(jntList), len(numList))
-for i in range(len(jntList)):
-    pm.parentConstraint(jntList[i], objList[i], w=1, mo=True)
-    pm.scaleConstraint(jntList[i], objList[i], w=1, mo=True)
+def temp6():
+    jntList = [f"jnt_{i}" for i in range(3, 298, 2)]
+    numList = [i for i in range(1, 149)]
+    numList = sorted(numList, reverse=True)
+    obj = "prop_bandolier_mdl_v9999:bandolier_magazine"
+    objList = [f"{obj}_{i}_grp" for i in numList]
+    print(jntList)
+    print(numList)
+    print(len(jntList), len(numList))
+    for i in range(len(jntList)):
+        pm.parentConstraint(jntList[i], objList[i], w=1, mo=True)
+        pm.scaleConstraint(jntList[i], objList[i], w=1, mo=True)
+
+
+def createExpression1(obj, num):
+    expr = f"float $bX = {obj}.translateX;\n"
+    expr += f"float $bY = {obj}.translateY;\n"
+    expr += f"float $bZ = {obj}.translateZ;\n"
+    expr += "float $cc10X = cc_10.translateX;\n"
+    expr += "float $cc10Y = cc_10.translateY;\n"
+    expr += "float $cc10Z = cc_10.translateZ;\n"
+    expr += "float $dist = `mag<<$bX-$cc10X, $bY-$cc10Y, $bZ-$cc10Z>>`;\n"
+    expr += f"if ($dist > {num}) " + "{\n"
+    expr += f"    {obj}.visibility = 0;\n"
+    expr += "}\n"
+    expr += "else {\n"
+    expr += f"    {obj}.visibility = 1;\n"
+    expr += "}\n"
+    pm.expression(s=expr, o='', ae=1, uc='all')
+
+
+def createExpression2(num):
+    name = "prop_bandolier_mdl_v9999:bandolier_magazine"
+    expr = f"float $loc = locator{num}.translateX;\n"
+    expr += "if ($loc > 0) {\n"
+    expr += f"    {name}_{num}_grp.visibility = 0;" + "} \n"
+    expr += "else {\n"
+    expr += f"    {name}_{num}_grp.visibility = 1;" + "}"
+    pm.expression(s=expr, o='', ae=1, uc='all')
+
+
+for i in range(1, 149):
+    createExpression2(i)
+
+
+def temp7():
+    jntList = [i for i in range(3, 298, 2)]
+    jntList.sort(reverse=True)
+    jntList = [f"jnt_{i}" for i in jntList]
+    for i in jntList:
+        loc = pm.spaceLocator()
+        pm.pointConstraint(i, loc, mo=False, w=1.0)
+        # pm.orientConstraint(i, loc, mo=True, w=1.0)
+
+
+
+
 
