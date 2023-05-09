369f0a255856a34af55363679186c26a73741eb1
hjk0314 <hjk0314@gmail.com>
2023-05-09 19:11:24 +0900

    test6.py update

---
+++ test6.py
@@ -0,0 +1,25 @@
+import datetime
+
+
+data = {
+    "hjk": {
+        "1978-3-14": ["born"], 
+        "1985-3-1": ["elementary school"], 
+        "1991-3-1": ["middle school"], 
+        "1994-3-1": ["high school"], 
+        "1997-3-1": ["reapplicant student"], 
+        "1998-3-1": ["university", "sunday school teacher"], 
+        "1998-9-2": ["army"], 
+        "2000-11-3": ["discharge from military service"], 
+        "2011-5-1": ["joined ANISTU company"], 
+        "2014-12-1": ["joined MOGRAPH company"], 
+        "2018-10-28": ["married"], 
+        "2020-3-30": ["child is born"], 
+        "2020-5-1": ["joined HOLOTIVE company"], 
+        "2021-6-1": ["joined XORBIS company"], 
+        "2022-2-23": ["joined MADMANPOST company"], 
+    }, 
+}
+
+
+print(data)
