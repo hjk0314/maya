496d18c18a01f44e46030f9bcb533516d74ce1f2
hjk0314 <hjk0314@gmail.com>
2023-08-14 19:21:55 +0900

    quickRig_humman.py

--- quickRig_human.py
+++ quickRig_human.py
@@ -1,172 +1,189 @@
 import pymel.core as pm
-import hjk
+
+
+mixamoBones = {
+    'Hips': (0.0, 98.223, 1.464), 
+    'Spine': (0.0, 107.814, 1.588), 
+    'Spine1': (0.0, 117.134, 0.203), 
+    'Spine2': (0.0, 125.82, -1.089), 
+    'Neck': (0.0, 141.589, -3.019), 
+    'Head': (0.0, 150.649, -1.431), 
+    'HeadTop_End': (0.0, 171.409, 5.635), 
+    'LeftShoulder': (4.305, 136.196, -3.124), 
+    'LeftArm': (19.934, 135.702, -5.494), 
+    'LeftForeArm': (42.774, 135.702, -6.376), 
+    'LeftHand': (63.913, 135.702, -6.131), 
+    'LeftHandThumb1': (65.761, 135.008, -2.444), 
+    'LeftHandThumb2': (68.495, 133.652, -0.242), 
+    'LeftHandThumb3': (70.727, 132.545, 1.556), 
+    'LeftHandThumb4': (72.412, 131.709, 2.913), 
+    'LeftHandIndex1': (71.683, 134.879, -2.495), 
+    'LeftHandIndex2': (74.972, 134.879, -2.495), 
+    'LeftHandIndex3': (77.576, 134.879, -2.495), 
+    'LeftHandIndex4': (80.181, 134.879, -2.495), 
+    'LeftHandMiddle1': (71.566, 134.682, -4.906), 
+    'LeftHandMiddle2': (75.085, 134.762, -4.906), 
+    'LeftHandMiddle3': (78.171, 134.832, -4.906), 
+    'LeftHandMiddle4': (81.57, 134.908, -4.906), 
+    'LeftHandRing1': (71.293, 134.575, -6.84), 
+    'LeftHandRing2': (74.241, 134.742, -6.84), 
+    'LeftHandRing3': (77.231, 134.912, -6.84), 
+    'LeftHandRing4': (80.134, 135.078, -6.84), 
+    'LeftHandPinky1': (70.702, 134.116, -8.847), 
+    'LeftHandPinky2': (73.811, 134.283, -8.847), 
+    'LeftHandPinky3': (75.625, 134.38, -8.847), 
+    'LeftHandPinky4': (77.461, 134.478, -8.847), 
+    'RightShoulder': (-4.305, 136.196, -3.124), 
+    'RightArm': (-21.859, 135.702, -5.585), 
+    'RightForeArm': (-42.316, 135.702, -6.381), 
+    'RightHand': (-63.913, 135.702, -6.131), 
+    'RightHandThumb1': (-65.761, 135.008, -2.444), 
+    'RightHandThumb2': (-68.495, 133.652, -0.242), 
+    'RightHandThumb3': (-70.727, 132.545, 1.556), 
+    'RightHandThumb4': (-72.412, 131.709, 2.913), 
+    'RightHandIndex1': (-71.683, 134.879, -2.495), 
+    'RightHandIndex2': (-74.972, 134.879, -2.495), 
+    'RightHandIndex3': (-77.576, 134.879, -2.495), 
+    'RightHandIndex4': (-80.181, 134.879, -2.495), 
+    'RightHandMiddle1': (-71.565, 134.682, -4.906), 
+    'RightHandMiddle2': (-75.085, 134.762, -4.906), 
+    'RightHandMiddle3': (-78.171, 134.832, -4.906), 
+    'RightHandMiddle4': (-81.569, 134.908, -4.906), 
+    'RightHandRing1': (-71.293, 134.575, -6.84), 
+    'RightHandRing2': (-74.24, 134.742, -6.84), 
+    'RightHandRing3': (-77.231, 134.912, -6.84), 
+    'RightHandRing4': (-80.134, 135.078, -6.84), 
+    'RightHandPinky1': (-70.702, 134.116, -8.847), 
+    'RightHandPinky2': (-73.811, 134.283, -8.847), 
+    'RightHandPinky3': (-75.625, 134.38, -8.847), 
+    'RightHandPinky4': (-77.461, 134.478, -8.847), 
+    'LeftUpLeg': (10.797, 91.863, -1.849), 
+    'LeftLeg': (10.797, 50.067, -0.255), 
+    'LeftFoot': (10.797, 8.223, -4.39), 
+    'LeftToeBase': (10.797, 0.001, 5.7), 
+    'LeftToe_End': (10.797, 0.0, 14.439), 
+    'RightUpLeg': (-10.797, 91.863, -1.849), 
+    'RightLeg': (-10.797, 50.066, -0.255), 
+    'RightFoot': (-10.797, 8.223, -4.39), 
+    'RightToeBase': (-10.797, 0.001, 5.7), 
+    'RightToe_End': (-10.797, 0.0, 14.439), 
+    }
+
+
+hierarchyGroup = {
+    "spineGroup": [
+        'Hips', 
+        'Spine', 'Spine1', 'Spine2', 
+        'Neck', 
+        'Head', 'HeadTop_End'
+        ], 
+    "L_armGroup": [
+        'LeftShoulder', 
+        'LeftArm', 
+        'LeftForeArm', 
+        'LeftHand'
+        ], 
+    "R_armGroup": [
+        'RightShoulder', 
+        'RightArm', 
+        'RightForeArm', 
+        'RightHand'
+        ], 
+    "L_legGroup": [
+        'LeftUpLeg', 
+        'LeftLeg', 
+        'LeftFoot', 
+        'LeftToeBase', 
+        'LeftToe_End'
+        ], 
+    "R_legGroup": [
+        'RightUpLeg', 
+        'RightLeg', 
+        'RightFoot', 
+        'RightToeBase', 
+        'RightToe_End'
+        ], 
+    "L_thumbGroup": ['LeftHandThumb%d'%i for i in range(1, 5)], 
+    "L_indexGroup": ['LeftHandIndex%d'%i for i in range(1, 5)], 
+    "L_middleGroup": ['LeftHandMiddle%d'%i for i in range(1, 5)], 
+    "L_ringGroup": ['LeftHandRing%d'%i for i in range(1, 5)], 
+    "L_pinkyGroup": ['LeftHandPinky%d'%i for i in range(1, 5)], 
+    "R_thumbGroup": ['RightHandThumb%d'%i for i in range(1, 5)], 
+    "R_indexGroup": ['RightHandIndex%d'%i for i in range(1, 5)], 
+    "R_middleGroup": ['RightHandMiddle%d'%i for i in range(1, 5)], 
+    "R_ringGroup": ['RightHandRing%d'%i for i in range(1, 5)], 
+    "R_pinkyGroup": ['RightHandPinky%d'%i for i in range(1, 5)], 
+}
+
+
+hierarchyGroup2 = {
+    'Spine2': ['LeftShoulder', 'RightShoulder'], 
+    'Hips': ['LeftUpLeg', 'RightUpLeg'], 
+    'LeftHand': [
+        'LeftHandThumb1', 
+        'LeftHandIndex1', 
+        'LeftHandMiddle1', 
+        'LeftHandRing1', 
+        'LeftHandPinky1'
+        ], 
+    'RightHand': [
+        'RightHandThumb1', 
+        'RightHandIndex1', 
+        'RightHandMiddle1', 
+        'RightHandRing1', 
+        'RightHandPinky1'
+        ], 
+}
+
+
+def setHierarchy(lst: list) -> None:
+    num = len(lst) - 1
+    for i in range(num):
+        pm.parent(lst[i+1], lst[i])
+
+
+def orientJoints(lst: list):
+    jntList = pm.ls(lst)
+    initJnt = jntList[0]
+    jntName = initJnt.name()
+    if "LeftShoulder" in jntName or "LeftHand" in jntName:
+        pri = 'yxz'
+        sec = 'zdown'
+    elif "RightShoulder" in jntName or "RightHand" in jntName:
+        pri = 'yxz'
+        sec = 'zup'
+    else:
+        pri = 'yzx'
+        sec = 'zup'
+    pm.makeIdentity(jntList, a=True, jo=True, n=0)
+    pm.joint(initJnt, e=True, oj=pri, sao=sec, ch=True, zso=True)
+    endJoint = [i for i in jntList if not i.getChildren()]
+    for i in endJoint:
+        pm.joint(i, e=True, oj='none', ch=True, zso=True)
+
+
+dag = pm.ls()
+jnt = 'Hips'
+cuv = 'mixamo_boneSample'
+if jnt in dag or cuv in dag:
+    print("There are sample joints or curves.")
+else:
+    for j, k in mixamoBones.items():
+        pm.select(cl=True)
+        pm.joint(p=k, n=j)
+    for i in hierarchyGroup.values():
+        setHierarchy(i)
+        orientJoints(i)
+    for j, k in hierarchyGroup2.items():
+        for i in k:
+            pm.parent(i, j)
+    cuv = pm.circle(nr=(0, 1, 0), n=cuv, ch=False, r=50)
+    pm.parent(jnt, cuv)
+    pm.select(cl=True)
 
 
 # 79 char line ================================================================
 # 72 docstring or comments line ========================================
 
 
-class QuickRig_Human:
-    def __init__(self):
-        self.circle = "humanCircle"
-        self.parentDict = {
-            "jnt_root": ['jnt_spine_1', 'jnt_hip_L', 'jnt_hip_R'], 
-            "jnt_spine_4": ['jnt_neck_1', 'jnt_scapula_L', 'jnt_scapula_R'], 
-            "jnt_neck_2": ['jnt_head'], 
-            "jnt_parm_L": [
-                'jnt_thumb_L_1', 
-                'jnt_index_L_1', 
-                'jnt_middle_L_1', 
-                'jnt_ring_L_1', 
-                'jnt_pinky_L_1', 
-            ], 
-            "jnt_parm_R": [
-                'jnt_thumb_R_1', 
-                'jnt_index_R_1', 
-                'jnt_middle_R_1', 
-                'jnt_ring_R_1', 
-                'jnt_pinky_R_1', 
-            ], 
-        }
-        self.point = {
-            'root': {
-                'jnt_root': (0.0, 91.146, -2.311)
-            }, 
-            'spine': {
-                'jnt_spine_1': (0.0, 101.79, -0.828), 
-                'jnt_spine_2': (-0.0, 112.04, -0.291), 
-                'jnt_spine_3': (-0.0, 121.978, -0.795), 
-                'jnt_spine_4': (-0.0, 130.703, -2.506), 
-            }, 
-            'neck': {
-                'jnt_neck_1': (-0.0, 139.579, -5.133), 
-                'jnt_neck_2': (-0.0, 143.202, -3.147), 
-            }, 
-            'head': {
-                'jnt_head': (-0.0, 151.415, -0.776), 
-                'jnt_head_end': (-0.0, 164.146, -0.776), 
-            }, 
-            'arm_L': {
-                'jnt_scapula_L': (7.581, 133.499, -4.058), 
-                'jnt_shoulder_L': (16.671, 129.738, -3.624), 
-                'jnt_elbow_L': (41.006, 127.424, -4.551), 
-                'jnt_wrist_L': (66.078, 125.041, -4.841), 
-                'jnt_parm_L': (71.356, 124.058, -4.841), 
-            }, 
-            'arm_R': {
-                'jnt_scapula_R': (-7.581, 133.499, -4.058), 
-                'jnt_shoulder_R': (-16.671, 129.738, -3.624), 
-                'jnt_elbow_R': (-41.006, 127.424, -4.551), 
-                'jnt_wrist_R': (-66.078, 125.041, -4.841), 
-                'jnt_parm_R': (-71.356, 124.058, -4.841), 
-            }, 
-            'leg_L': {
-                'jnt_hip_L': (10.968, 82.407, -0.453), 
-                'jnt_knee_L': (13.083, 45.307, -1.572), 
-                'jnt_ankle_L': (14.812, 8.647, -4.123), 
-                'jnt_ball_L': (17.597, 1.022, 5.062), 
-                'jnt_toe_L': (19.642, 0.075, 13.005), 
-            }, 
-            'leg_R': {
-                'jnt_hip_R': (-10.968, 82.407, -0.453), 
-                'jnt_knee_R': (-13.082, 45.307, -1.572), 
-                'jnt_ankle_R': (-14.812, 8.647, -4.123), 
-                'jnt_ball_R': (-17.597, 1.022, 5.062), 
-                'jnt_toe_R': (-19.642, 0.075, 13.005), 
-            }, 
-            'thumb_L': {
-                'jnt_thumb_L_1': (69.91, 123.216, -2.033), 
-                'jnt_thumb_L_2': (71.54, 122.745, -0.221), 
-                'jnt_thumb_L_3': (73.169, 122.275, 1.591), 
-                'jnt_thumb_L_4': (74.799, 121.804, 3.403), 
-            }, 
-            'index_L': {
-                'jnt_index_L_1': (74.697, 123.363, -2.847), 
-                'jnt_index_L_2': (77.057, 123.282, -2.082), 
-                'jnt_index_L_3': (79.417, 123.2, -1.318), 
-                'jnt_index_L_4': (81.777, 123.119, -0.554), 
-            }, 
-            'middle_L': {
-                'jnt_middle_L_1': (75.236, 123.622, -4.757), 
-                'jnt_middle_L_2': (77.687, 123.519, -5.131), 
-                'jnt_middle_L_3': (80.139, 123.415, -5.505), 
-                'jnt_middle_L_4': (82.59, 123.312, -5.88), 
-            }, 
-            'ring_L': {
-                'jnt_ring_L_1': (74.493, 123.638, -6.872), 
-                'jnt_ring_L_2': (76.437, 123.465, -8.164), 
-                'jnt_ring_L_3': (78.381, 123.293, -9.456), 
-                'jnt_ring_L_4': (80.324, 123.12, -10.749), 
-            }, 
-            'pinky_L': {
-                'jnt_pinky_L_1': (72.929, 123.458, -8.344), 
-                'jnt_pinky_L_2': (73.715, 123.357, -10.018), 
-                'jnt_pinky_L_3': (74.501, 123.257, -11.692), 
-                'jnt_pinky_L_4': (75.286, 123.158, -13.366), 
-            }, 
-            'thumb_R': {
-                'jnt_thumb_R_1': (-69.91, 123.216, -2.033), 
-                'jnt_thumb_R_2': (-71.54, 122.745, -0.221), 
-                'jnt_thumb_R_3': (-73.169, 122.275, 1.591), 
-                'jnt_thumb_R_4': (-74.799, 121.804, 3.403), 
-            }, 
-            'index_R': {
-                'jnt_index_R_1': (-74.697, 123.363, -2.847), 
-                'jnt_index_R_2': (-77.057, 123.282, -2.082), 
-                'jnt_index_R_3': (-79.417, 123.2, -1.318), 
-                'jnt_index_R_4': (-81.777, 123.119, -0.554), 
-            }, 
-            'middle_R': {
-                'jnt_middle_R_1': (-75.236, 123.622, -4.757), 
-                'jnt_middle_R_2': (-77.687, 123.519, -5.131), 
-                'jnt_middle_R_3': (-80.139, 123.415, -5.505), 
-                'jnt_middle_R_4': (-82.59, 123.312, -5.88), 
-            }, 
-            'ring_R': {
-                'jnt_ring_R_1': (-74.493, 123.638, -6.872), 
-                'jnt_ring_R_2': (-76.437, 123.465, -8.164), 
-                'jnt_ring_R_3': (-78.381, 123.293, -9.456), 
-                'jnt_ring_R_4': (-80.324, 123.12, -10.749), 
-            }, 
-            'pinky_R': {
-                'jnt_pinky_R_1': (-72.929, 123.458, -8.344), 
-                'jnt_pinky_R_2': (-73.715, 123.357, -10.018), 
-                'jnt_pinky_R_3': (-74.501, 123.257, -11.692), 
-                'jnt_pinky_R_4': (-75.285, 123.158, -13.366), 
-            }, 
-        }
 
-
-    def createTempJoints(self):
-        try: pm.delete(self.circle)
-        except: pass
-        humanJnt = self.point.values()
-        branch = []
-        for i in humanJnt:
-            pm.select(cl=True)
-            for name, pos in i.items():
-                jnt = pm.joint(p=(0, 0, 0), n=name, rad=10)
-                pm.move(jnt, pos)
-            headJnt = [j for j in i.keys()][0]
-            branch.append(headJnt)
-        hjk.orientJnt(branch)
-        self.circle = hjk.createCircle(self.circle, 60, y=True)
-        tmp = [pm.parent(k, j) for j, k in self.parentDict.items()]
-        pm.parent(branch[0], self.circle)
-
-
-    def createCtrls(self):
-        pass
-        # self.createMainCtrl()
-        # self.createRootCtrl()
-        # self.createSpineCtrl()
-        # self.createNeckCtrl()
-        # self.createHeadCtrl()
-        # self.createEyeCtrl()
-        # self.createArmCtrl()
-        # self.createLegCtrl()
-        # self.createFingerCtrl()
-
-
-human = QuickRig_Human()
-human.createTempJoints()
