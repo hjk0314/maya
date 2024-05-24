72c11ee39f1c13b80c590bf465b33543e6579a8d
hjk0314 <hjk0314@gmail.com>
2024-05-24 19:37:42 +0900

    starting Car() class in test1.py

--- test1.py
+++ test1.py
@@ -14,6 +14,9 @@ def mayaMainWindow():
 
 class Car(QWidget):
     def __init__(self):
+        self.rootJnt = "jnt_root"
+        self.rootFbx = "fbx_root"
+        self.mainCtrl = "cc_main"
         self.ctrlNames = {
             "car": "cc_body", 
             "car2": "cc_sub", 
@@ -52,6 +55,64 @@ class Car(QWidget):
         pass
 
 
+    def build(self):
+        self.updateJointsPosition()
+        # self.updateSameSide()
+        self.cleanUp()
+        self.createGroups()
+        self.createJoints()
+        self.createFbxJoints()
+        self.createCtrls()
+        # self.setControllers()
+
+
+    def updateJointsPosition(self):
+        for jnt in self.jntNameAndPos.keys():
+            pos = getPosition(jnt)
+            self.jntNameAndPos[jnt] = pos
+
+
+    def updateSameSide(self, side: str="LeftToRight"):
+        """ The default change is from left to right. 
+        But the opposite is also possible.
+        >>> updateSameSide()
+        >>> updateSameSide("RightToLeft")
+         """
+        A, B = side.split("To")
+        aSide = []
+        bSide = []
+        for jntName in self.jntNameAndPos.keys():
+            if A in jntName:
+                aSide.append(jntName)
+            elif B in jntName:
+                bSide.append(jntName)
+            else:
+                continue
+        for idx, aJoint in enumerate(aSide):
+            x, y, z = pm.xform(aJoint, q=True, t=True, ws=True)
+            bJoint = bSide[idx]
+            self.jntNameAndPos[bJoint] = (-1*x, y, z)
+
+
+    def cleanUp(self):
+        listDelete = [
+            self.rootJnt, 
+            self.rootFbx, 
+            self.mainCtrl + "_grp", 
+            ]
+        for i in listDelete:
+            try:
+                pm.delete(i)
+            except:
+                continue
+
+
+    def createGroups(self):
+        carName = "test"
+        rg = RigGroups()
+        rg.createRigGroups(carName)
+
+
     def createJoints(self):
         for jnt, pos in self.jntNameAndPos.items():
             pm.select(cl=True)
@@ -61,32 +122,44 @@ class Car(QWidget):
                 parentHierarchically(*children)
                 pm.makeIdentity(children, a=1, t=1, r=1, s=1, jo=1)
                 pm.parent(children[0], parents)
-
-
-    def build(self):
-        self.updatePosition()
-        self.cleanUp()
-        self.createJoints()
-        self.createFbxJoints()
-        self.createCtrls(**self.ctrlNames)
-        self.setControllers()
-
-
-    def updateJointsPosition(self):
-        for jnt in self.jntNameAndPos.keys():
-            pos = getPosition(jnt)
-            self.jntNameAndPos[jnt] = pos
+        self.tryParent(self.rootJnt, "bindBones")
 
 
     def createFbxJoints(self):
-        jntList = list(self.jntNameAndPos.keys())
-        jnt = jntList[0]
-        fbx = jnt.replace("jnt_", "fbx_")
-        fbx = pm.duplicate(jnt, rr=True, n=fbx)[0]
-        for i in selectJointOnly():
+        fbx = pm.duplicate(self.rootJnt, rr=True, n=self.rootFbx)
+        fbx = fbx[0]
+        pm.select(cl=True)
+        pm.select(fbx, hi=True)
+        for i in pm.ls(sl=True):
             new = i.replace("jnt_", "fbx_")
             pm.rename(i, new)
+        self.tryParent(self.rootFbx, "rigBones")
 
 
+    def createCtrls(self):
+        cc = Controllers()
+        cc.createControllers(**self.ctrlNames)
+        ctrl = list(self.ctrlNames.values())
+        grps = []
+        for i in ctrl:
+            if self.mainCtrl == i:
+                pm.scale(i, (20, 20, 20))
+                pm.makeIdentity(i, a=1, t=0, r=0, s=1, n=0, pn=1)
+            grp = groupingWithOwnPivot(i)[0]
+            grps.append(grp)
+        for child, parents in zip(grps[:2], ctrl[1:]):
+            pm.parent(child, parents)
+        pm.matchTransform(grps[0], "jnt_body", pos=True)
+        self.tryParent(self.mainCtrl + "_grp", "controllers")
+        
+
+    def tryParent(self, child: str, parents: str):
+        try:
+            pm.parent(child, parents)
+        except:
+            pass
 
 
+car = Car()
+# car.createJoints()
+car.build()
