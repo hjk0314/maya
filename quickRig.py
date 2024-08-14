2eed50476d1494728a04b78cb592740d17f5289b
hjk0314 <hjk0314@gmail.com>
2024-08-14 20:15:18 +0900

    add mixamo class in quickRig.py

--- quickRig.py
+++ quickRig.py
@@ -783,13 +783,380 @@ class Car(QWidget):
                     continue
 
 
-if __name__ == "__main__":
-    try:
-        qrCar.close()
-        qrCar.deleteLater()
-    except:
+# if __name__ == "__main__":
+#     try:
+#         qrCar.close()
+#         qrCar.deleteLater()
+#     except:
+#         pass
+#     qrCar = Car()
+#     qrCar.show()
+
+
+class MixamoCharacter(QWidget):
+    def __init__(self):
+        self.mainCurve = "mainCurve"
+        self.spine = [
+            "Hips", 
+            "Spine", 
+            "Spine1", 
+            "Spine2", 
+            "Neck", 
+            "Head", 
+            "HeadTop_End"
+            ]
+        self.leftArms = [
+            "LeftShoulder", 
+            "LeftArm", 
+            "LeftForeArm", 
+            "LeftHand"
+            ]
+        self.leftLegs = [
+            "LeftUpLeg", 
+            "LeftLeg", 
+            "LeftFoot", 
+            "LeftToeBase", 
+            "LeftToe_End"
+            ]
+        self.leftThumb = [
+            "LeftHandThumb1", 
+            "LeftHandThumb2", 
+            "LeftHandThumb3", 
+            "LeftHandThumb4"
+            ]
+        self.leftIndex = [
+            "LeftHandIndex1", 
+            "LeftHandIndex2", 
+            "LeftHandIndex3", 
+            "LeftHandIndex4"
+            ]
+        self.leftMiddle = [
+            "LeftHandMiddle1", 
+            "LeftHandMiddle2", 
+            "LeftHandMiddle3", 
+            "LeftHandMiddle4"
+            ]
+        self.leftRing = [
+            "LeftHandRing1", 
+            "LeftHandRing2", 
+            "LeftHandRing3", 
+            "LeftHandRing4"
+            ]
+        self.leftPinky = [
+            "LeftHandPinky1", 
+            "LeftHandPinky2", 
+            "LeftHandPinky3", 
+            "LeftHandPinky4"
+            ]
+        self.rightArms = [
+            "RightShoulder", 
+            "RightArm", 
+            "RightForeArm", 
+            "RightHand"
+            ]
+        self.rightLegs = [
+            "RightUpLeg", 
+            "RightLeg", 
+            "RightFoot", 
+            "RightToeBase", 
+            "RightToe_End"
+            ]
+        self.rightThumb = [
+            "RightHandThumb1", 
+            "RightHandThumb2", 
+            "RightHandThumb3", 
+            "RightHandThumb4"
+            ]
+        self.rightIndex = [
+            "RightHandIndex1", 
+            "RightHandIndex2", 
+            "RightHandIndex3", 
+            "RightHandIndex4"
+            ]
+        self.rightMiddle = [
+            "RightHandMiddle1", 
+            "RightHandMiddle2", 
+            "RightHandMiddle3", 
+            "RightHandMiddle4"
+            ]
+        self.rightRing = [
+            "RightHandRing1", 
+            "RightHandRing2", 
+            "RightHandRing3", 
+            "RightHandRing4"
+            ]
+        self.rightPinky = [
+            "RightHandPinky1", 
+            "RightHandPinky2", 
+            "RightHandPinky3", 
+            "RightHandPinky4"
+            ]
+        self.jointPosition = {
+            "Hips": (0.0, 98.223, 1.464), 
+            "Spine": (0.0, 107.814, 1.588), 
+            "Spine1": (0.0, 117.134, 0.203), 
+            "Spine2": (0.0, 125.82, -1.089), 
+            "Neck": (0.0, 141.589, -3.019), 
+            "Head": (0.0, 150.649, -1.431), 
+            "HeadTop_End": (0.0, 171.409, 5.635), 
+            "LeftShoulder": (4.305, 136.196, -3.124), 
+            "LeftArm": (19.934, 135.702, -5.494), 
+            "LeftForeArm": (42.774, 135.702, -6.376), 
+            "LeftHand": (63.913, 135.702, -6.131), 
+            "LeftHandThumb1": (65.761, 135.008, -2.444), 
+            "LeftHandThumb2": (68.495, 133.652, -0.242), 
+            "LeftHandThumb3": (70.727, 132.545, 1.556), 
+            "LeftHandThumb4": (72.412, 131.709, 2.913), 
+            "LeftHandIndex1": (71.683, 134.879, -2.495), 
+            "LeftHandIndex2": (74.972, 134.879, -2.495), 
+            "LeftHandIndex3": (77.576, 134.879, -2.495), 
+            "LeftHandIndex4": (80.181, 134.879, -2.495), 
+            "LeftHandMiddle1": (71.566, 134.682, -4.906), 
+            "LeftHandMiddle2": (75.085, 134.762, -4.906), 
+            "LeftHandMiddle3": (78.171, 134.832, -4.906), 
+            "LeftHandMiddle4": (81.57, 134.908, -4.906), 
+            "LeftHandRing1": (71.293, 134.575, -6.84), 
+            "LeftHandRing2": (74.241, 134.742, -6.84), 
+            "LeftHandRing3": (77.231, 134.912, -6.84), 
+            "LeftHandRing4": (80.134, 135.078, -6.84), 
+            "LeftHandPinky1": (70.702, 134.116, -8.847), 
+            "LeftHandPinky2": (73.811, 134.283, -8.847), 
+            "LeftHandPinky3": (75.625, 134.38, -8.847), 
+            "LeftHandPinky4": (77.461, 134.478, -8.847), 
+            "RightShoulder": (-4.305, 136.196, -3.124), 
+            "RightArm": (-21.859, 135.702, -5.585), 
+            "RightForeArm": (-42.316, 135.702, -6.381), 
+            "RightHand": (-63.913, 135.702, -6.131), 
+            "RightHandThumb1": (-65.761, 135.008, -2.444), 
+            "RightHandThumb2": (-68.495, 133.652, -0.242), 
+            "RightHandThumb3": (-70.727, 132.545, 1.556), 
+            "RightHandThumb4": (-72.412, 131.709, 2.913), 
+            "RightHandIndex1": (-71.683, 134.879, -2.495), 
+            "RightHandIndex2": (-74.972, 134.879, -2.495), 
+            "RightHandIndex3": (-77.576, 134.879, -2.495), 
+            "RightHandIndex4": (-80.181, 134.879, -2.495), 
+            "RightHandMiddle1": (-71.565, 134.682, -4.906), 
+            "RightHandMiddle2": (-75.085, 134.762, -4.906), 
+            "RightHandMiddle3": (-78.171, 134.832, -4.906), 
+            "RightHandMiddle4": (-81.569, 134.908, -4.906), 
+            "RightHandRing1": (-71.293, 134.575, -6.84), 
+            "RightHandRing2": (-74.24, 134.742, -6.84), 
+            "RightHandRing3": (-77.231, 134.912, -6.84), 
+            "RightHandRing4": (-80.134, 135.078, -6.84), 
+            "RightHandPinky1": (-70.702, 134.116, -8.847), 
+            "RightHandPinky2": (-73.811, 134.283, -8.847), 
+            "RightHandPinky3": (-75.625, 134.38, -8.847), 
+            "RightHandPinky4": (-77.461, 134.478, -8.847), 
+            "LeftUpLeg": (10.797, 91.863, -1.849), 
+            "LeftLeg": (10.797, 50.067, -0.255), 
+            "LeftFoot": (10.797, 8.223, -4.39), 
+            "LeftToeBase": (10.797, 0.001, 5.7), 
+            "LeftToe_End": (10.797, 0.0, 14.439), 
+            "RightUpLeg": (-10.797, 91.863, -1.849), 
+            "RightLeg": (-10.797, 50.066, -0.255), 
+            "RightFoot": (-10.797, 8.223, -4.39), 
+            "RightToeBase": (-10.797, 0.001, 5.7), 
+            "RightToe_End": (-10.797, 0.0, 14.439), 
+            }
+        self.hierarchy = {
+            "Hips": [self.spine[1:], self.leftLegs, self.rightLegs], 
+            "Spine2": [self.leftArms, self.rightArms], 
+            "LeftHand": [
+                self.leftThumb, 
+                self.leftIndex, 
+                self.leftMiddle, 
+                self.leftRing, 
+                self.leftPinky
+                ], 
+            "RightHand": [
+                self.rightThumb, 
+                self.rightIndex, 
+                self.rightMiddle, 
+                self.rightRing, 
+                self.rightPinky
+                ], 
+            }
+        # self.setupUI()
+    
+
+    def setupUI(self):
+        # super(Car, self).__init__()
+        # self.setParent(mayaMainWindow())
+        # self.setWindowFlags(Qt.Window)
+        self.buttonsLink()
+
+
+    def buttonsLink(self):
         pass
-    qrCar = Car()
-    qrCar.show()
 
 
+    def createBones(self):
+        self.cleanUp(self.mainCurve, self.jointPosition.keys())
+        self.createJointAndNameIt(self.jointPosition)
+        self.buildHierarchy(self.hierarchy)
+        self.createMainCurve()
+
+
+    def alignBonesCenter(self):
+        self.updateAllJointPositions()
+        self.updatePositionGridCenter(self.spine)
+        self.createBones()
+
+
+    def alignBonesSameSide(self):
+        AtoB = "LeftToRight"
+        # AtoB = "RightToLeft"
+        self.updateAllJointPositions()
+        sideA, sideB = self.seperateLeftAndRight(AtoB)
+        self.updateBothSideToSame(sideA, sideB)
+        self.createBones()
+
+
+    def createRig_All(self):
+        self.updateAllJointPositions()
+        jntPos = self.jointPosition
+        hiraky = self.hierarchy
+        data = self.copyBonesForRig(jntPos, hiraky, "rig_", "")
+        rigJntPos, rigHiraky = data
+        self.cleanUp(rigJntPos.keys())
+        self.createJointAndNameIt(rigJntPos)
+        self.buildHierarchy(rigHiraky)
+
+
+    def createRig_IKFK(self):
+        self.createIKFK(self.spine[0], self.spine[1:4])
+        self.createIKFK(self.leftArms[0], self.leftArms[1:])
+        self.createIKFK(self.rightArms[0], self.rightArms[1:])
+        self.createIKFK(self.spine[0], self.leftLegs)
+        self.createIKFK(self.spine[0], self.rightLegs)
+
+
+# ==============================================================================
+
+
+    def createIKFK(self, parents: str, joints: list):
+        self.updateAllJointPositions()
+        jntPos = {i: self.jointPosition[i] for i in joints}
+        hiraky = {parents: [joints]}
+        for i in ["_IK", "_FK"]:
+            data = self.copyBonesForRig(jntPos, hiraky, "rig_", i)
+            rigJntPos, temp = data
+            rigHiraky = {k.rsplit("_", 1)[0]: v for k, v in temp.items()}
+            self.cleanUp(rigJntPos.keys())
+            self.createJointAndNameIt(rigJntPos)
+            self.buildHierarchy(rigHiraky)
+
+
+    def copyBonesForRig(self, jointPosition, hierarchy, fore="", tail=""):
+        """ Returns the data with a new name. 
+        - positions = {str: (float, float, float), ...}
+        - hierarchy = {str: [[ ],[ ]], ...}
+         """
+        rigJointPosition = {}
+        for jnt, pos in jointPosition.items():
+            rigJointPosition[f"{fore}{jnt}{tail}"] = pos 
+        rigHierarchy = {}
+        for parents, children in hierarchy.items():
+            key = f"{fore}{parents}{tail}"
+            value = [[f"{fore}{j}{tail}" for j in i] for i in children]
+            rigHierarchy[key] = value
+        return rigJointPosition, rigHierarchy
+
+
+    def updateBothSideToSame(self, sideA, sideB):
+        for idx, joint in enumerate(sideA):
+            x, y, z = pm.xform(joint, q=True, t=True, ws=True)
+            self.jointPosition[sideB[idx]] = (x*-1, y, z)
+
+
+    def seperateLeftAndRight(self, twoOptions: str) -> list:
+        """ Direction has one of the options: 
+        >>> "LeftToRight" or "RightToLeft" 
+         """
+        allJoints = self.jointPosition.keys()
+        A, B = twoOptions.split("To")
+        side = []
+        otherSide = []
+        for jointName in allJoints:
+            if A in jointName:
+                side.append(jointName)
+            elif B in jointName:
+                otherSide.append(jointName)
+            else:
+                continue
+        return side, otherSide
+
+
+    def updateAllJointPositions(self):
+        allJoints = self.jointPosition.keys()
+        for joint in allJoints:
+            position = pm.xform(joint, q=True, t=True, ws=True)
+            self.jointPosition[joint] = tuple(position)
+
+
+    def updatePositionGridCenter(self, joints: list):
+        for joint in joints:
+            x, y, z = pm.xform(joint, q=True, t=True, ws=True)
+            self.jointPosition[joint] = (0, y, z)
+
+
+    def createMainCurve(self):
+        rootJnt = self.spine[0]
+        if not pm.objExists(rootJnt):
+            return
+        else:
+            bbSize = getBoundingBoxSize(rootJnt)
+            pm.circle(nr=(0, 1, 0), n=self.mainCurve, ch=0, r=bbSize)
+            pm.parent(rootJnt, self.mainCurve)
+
+
+    def getPrimaryAndSecondaryAxis(self, jnt=[]):
+        isLeft = any(i in jnt[0] for i in self.leftArms)
+        isRight = any(i in jnt[0] for i in self.rightArms)
+        if isLeft:
+            primaryAxis = 'yxz'
+            secondaryAxis = 'zdown'
+        elif isRight:
+            primaryAxis = 'yxz'
+            secondaryAxis = 'zup'
+        else:
+            primaryAxis = 'yzx'
+            secondaryAxis = 'zup'
+        return primaryAxis, secondaryAxis
+
+
+    def buildHierarchy(self, hierarchyStructure: dict):
+        for parents, bothSideList in hierarchyStructure.items():
+            for jointList in bothSideList:
+                parentHierarchically(*jointList)
+                priAxis, secAxis = self.getPrimaryAndSecondaryAxis(jointList)
+                orientJoints(jointList, priAxis, secAxis)
+                parentHierarchically(*[parents, jointList[0]])
+
+
+    def createJointAndNameIt(self, nameAndPosition: dict):
+        for jointName, position in nameAndPosition.items():
+            pm.select(cl=True)
+            pm.joint(p=position, n=jointName)
+
+
+    def cleanUp(self, *args):
+        for element in args:
+            isStr = isinstance(element, str)
+            isIter = isinstance(element, Iterable)
+            if not isStr and isIter:
+                for i in element:
+                    self.cleanUp(i)
+            else:
+                try:
+                    pm.delete(element)
+                except:
+                    pass
+
+
+# mc = MixamoCharacter()
+# mc.createBones()
+# mc.alignBonesCenter()
+# mc.alignBonesSameSide()
+# mc.createRig_All()
+# mc.createRig_IKFK()
