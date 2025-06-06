247daeee7c9559247898256bacc06712338a575d
hjk0314 <87267837+hjk0314@users.noreply.github.com>
2025-06-07 00:53:13 +0900

    Add utility to query UV coordinates

---
+++ uv_tools.py
@@ -0,0 +1,58 @@
+"""Utilities for retrieving UV coordinates using PyMel."""
+
+import pymel.core as pm
+
+
+def get_component_uv_coordinates(component):
+    """Return the UV coordinates for a mesh component.
+
+    Parameters
+    ----------
+    component : str or pm.PyNode
+        Mesh component such as a face, vertex, edge or uv.
+
+    Returns
+    -------
+    list of tuple
+        List of ``(u, v)`` values for each UV found on the component.
+    """
+    uv_comp = pm.polyListComponentConversion(component, toUV=True)
+    uv_comp = pm.ls(uv_comp, fl=True)
+
+    result = []
+    for uv in uv_comp:
+        u, v = pm.polyEditUV(uv, query=True)
+        result.append((u, v))
+    return result
+
+
+def get_uv_coords_from_faces(mesh, face_indices, uv_set=None):
+    """Collect UV coordinates for specific faces of a mesh.
+
+    Parameters
+    ----------
+    mesh : str or pm.nt.Mesh
+        Target mesh object or its transform.
+    face_indices : iterable of int
+        The face indices to query.
+    uv_set : str, optional
+        Name of the UV set. If ``None`` the current UV set is used.
+
+    Returns
+    -------
+    dict
+        Mapping of face index to a list of ``(u, v)`` coordinates.
+    """
+    node = pm.PyNode(mesh)
+    shape = node.getShape() if node.nodeType() != 'mesh' else node
+
+    if uv_set is None:
+        uv_set = shape.getCurrentUVSetName()
+
+    result = {}
+    for index in face_indices:
+        face = shape.f[index]
+        uv_coords = get_component_uv_coordinates(face)
+        result[index] = uv_coords
+    return result
+
