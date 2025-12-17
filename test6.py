ad3ebb020a22e94a80eb764c6962513ed064e81e
hjk0314 <hjk0314@gmail.com>
2025-12-17 19:37:16 +0900

    start to edit neck

---
+++ test6.py
@@ -0,0 +1,25 @@
+import maya.cmds as cmds
+import hjk3
+
+
+
+
+# hjk3.group_with_pivot(null=True)
+
+
+# cpu = hjk3.ColorPickerUI()
+# cpu.show()
+
+
+# sel = ["new_neck_%d" % i for i in range(1, 9)]
+# hjk3.create_curve_ikSpline(*sel)
+
+
+
+# hjk3.create_ikSplineHandle("curve3", sel, sy=True)
+
+
+# sel = cmds.ls(sl=True)
+# val1, val2 = hjk3.get_constraint_weight_by_distance(sel[0], sel[1], sel[2])
+# cmds.parentConstraint(sel[0], sel[2], mo=True, w=val1)
+# cmds.parentConstraint(sel[1], sel[2], mo=True, w=val2)
