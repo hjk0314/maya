02c1a1dbb1eb88edb58cf5b8f495714f0f37df51
hjk0314 <hjk0314@gmail.com>
2025-12-29 17:11:44 +0900

    select upper ctrls

--- test8.py
+++ test8.py
@@ -26,3 +26,18 @@ def create_show_and_hide_channel_and_connect():
     sel = cmds.ls(sl=True)
     for i in sel:
         cmds.connectAttr(f"{cc_main}.{attr_name}", f"{i}.visibility", f=True)
+
+
+
+sel = cmds.ls(sl=True)
+for i in sel:
+    shapes = cmds.ls(i, dag=True, type="nurbsCurve")
+    shapes = [shp for shp in shapes]
+    result = []
+    for shp in shapes:
+        obj = cmds.listRelatives(shp, p=True)[0]
+        result.append(obj)
+    result.reverse()
+    cmds.select(cl=True)
+    cmds.select(result)
+    
