67240ecf8808b2bb520b4d45275603b0633ba62c
hjk0314 <hjk0314@gmail.com>
2025-12-31 17:00:24 +0900

    finish crown of peacock

--- test8.py
+++ test8.py
@@ -29,15 +29,47 @@ def create_show_and_hide_channel_and_connect():
 
 
 
-sel = cmds.ls(sl=True)
-for i in sel:
-    shapes = cmds.ls(i, dag=True, type="nurbsCurve")
-    shapes = [shp for shp in shapes]
-    result = []
-    for shp in shapes:
-        obj = cmds.listRelatives(shp, p=True)[0]
-        result.append(obj)
-    result.reverse()
-    cmds.select(cl=True)
-    cmds.select(result)
+# sel = cmds.ls(sl=True)
+# for i in sel:
+#     shapes = cmds.ls(i, dag=True, type="nurbsCurve")
+#     shapes = [shp for shp in shapes]
+#     result = []
+#     for shp in shapes:
+#         obj = cmds.listRelatives(shp, p=True)[0]
+#         result.append(obj)
+#     result.reverse()
+#     cmds.select(cl=True)
+#     cmds.select(result)
     
+
+
+
+sel = cmds.ls(sl=True)
+# print(sel)
+# sel = ['char_peacockA_mdl_v9999:peacockA_crown_01_rachii_geo', 'char_peacockA_mdl_v9999:peacockA_crown_01_feather_geo', 'char_peacockA_mdl_v9999:peacockA_crown_02_rachii_geo', 'char_peacockA_mdl_v9999:peacockA_crown_02_feather_geo', 'char_peacockA_mdl_v9999:peacockA_crown_03_rachii_geo', 'char_peacockA_mdl_v9999:peacockA_crown_03_feather_geo', 'char_peacockA_mdl_v9999:peacockA_crown_04_rachii_geo', 'char_peacockA_mdl_v9999:peacockA_crown_04_feather_geo', 'char_peacockA_mdl_v9999:peacockA_crown_05_rachii_geo', 'char_peacockA_mdl_v9999:peacockA_crown_05_feather_geo', 'char_peacockA_mdl_v9999:peacockA_crown_06_rachii_geo', 'char_peacockA_mdl_v9999:peacockA_crown_06_feather_geo', 'char_peacockA_mdl_v9999:peacockA_crown_07_rachii_geo', 'char_peacockA_mdl_v9999:peacockA_crown_07_feather_geo', 'char_peacockA_mdl_v9999:peacockA_crown_08_rachii_geo', 'char_peacockA_mdl_v9999:peacockA_crown_08_feather_geo', 'char_peacockA_mdl_v9999:peacockA_crown_09_rachii_geo', 'char_peacockA_mdl_v9999:peacockA_crown_09_feather_geo', 'char_peacockA_mdl_v9999:peacockA_crown_10_rachii_geo', 'char_peacockA_mdl_v9999:peacockA_crown_10_feather_geo', 'char_peacockA_mdl_v9999:peacockA_crown_11_rachii_geo', 'char_peacockA_mdl_v9999:peacockA_crown_11_feather_geo']
+
+# rachiis = sel[::2]
+# feathers = sel[1::2]
+# for r, f in zip(rachiis, feathers):
+#     rac = f"{r}.vtx[9]"
+#     fea = f"{f}.vtx[10]"
+#     pos = hjk3.get_position(rac, fea)
+#     cuv = hjk3.create_curve_from_points(*pos)
+#     joints = hjk3.create_joint_on_curve_path(c=cuv, n=5)
+#     hjk3.parent_in_sequence(*joints)
+
+
+# ctrls = hjk3.create_FK_ctrls(cs=0.5, ds=0.125)
+# hjk3.group_with_pivot(*ctrls, null=True)
+
+
+# hjk3.replace_name(s="crown_", r="rig_crown_")
+
+
+# for cc in sel:
+#     jnt = cc.replace("cc_", "rig_")
+#     cmds.parentConstraint(cc, jnt, mo=True, w=1.0)
+
+
+# cpu = hjk3.ColorPickerUI()
+# cpu.show()
