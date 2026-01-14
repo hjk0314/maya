ec7ac2121e9ecdc7007d2cd61ac6f5ec4f448baa
hjk0314 <hjk0314@gmail.com>
2026-01-14 19:21:09 +0900

    edit restore_range_path()

--- hjk3.py
+++ hjk3.py
@@ -327,7 +327,73 @@ class Data:
                 (-25, 0, 25), (25, 0, 25)
                 ], 
         }
-        self.char_joints = {}
+        self.mixamo_joints = {
+            "Hips": (0.0, 98.223, 1.464), 
+            "Spine": (0.0, 107.814, 1.588), 
+            "Spine1": (0.0, 117.134, 0.203), 
+            "Spine2": (0.0, 125.82, -1.089), 
+            "Neck": (0.0, 141.589, -3.019), 
+            "Head": (0.0, 150.649, -1.431), 
+            "HeadTop_End": (0.0, 171.409, 5.635), 
+            "LeftShoulder": (4.305, 136.196, -3.124), 
+            "LeftArm": (19.934, 135.702, -5.494), 
+            "LeftForeArm": (42.774, 135.702, -6.376), 
+            "LeftHand": (63.913, 135.702, -6.131), 
+            "LeftHandThumb1": (65.761, 135.008, -2.444), 
+            "LeftHandThumb2": (68.495, 133.652, -0.242), 
+            "LeftHandThumb3": (70.727, 132.545, 1.556), 
+            "LeftHandThumb4": (72.412, 131.709, 2.913), 
+            "LeftHandIndex1": (71.683, 134.879, -2.495), 
+            "LeftHandIndex2": (74.972, 134.879, -2.495), 
+            "LeftHandIndex3": (77.576, 134.879, -2.495), 
+            "LeftHandIndex4": (80.181, 134.879, -2.495), 
+            "LeftHandMiddle1": (71.566, 134.682, -4.906), 
+            "LeftHandMiddle2": (75.085, 134.762, -4.906), 
+            "LeftHandMiddle3": (78.171, 134.832, -4.906), 
+            "LeftHandMiddle4": (81.57, 134.908, -4.906), 
+            "LeftHandRing1": (71.293, 134.575, -6.84), 
+            "LeftHandRing2": (74.241, 134.742, -6.84), 
+            "LeftHandRing3": (77.231, 134.912, -6.84), 
+            "LeftHandRing4": (80.134, 135.078, -6.84), 
+            "LeftHandPinky1": (70.702, 134.116, -8.847), 
+            "LeftHandPinky2": (73.811, 134.283, -8.847), 
+            "LeftHandPinky3": (75.625, 134.38, -8.847), 
+            "LeftHandPinky4": (77.461, 134.478, -8.847), 
+            "RightShoulder": (-4.305, 136.196, -3.124), 
+            "RightArm": (-21.859, 135.702, -5.585), 
+            "RightForeArm": (-42.316, 135.702, -6.381), 
+            "RightHand": (-63.913, 135.702, -6.131), 
+            "RightHandThumb1": (-65.761, 135.008, -2.444), 
+            "RightHandThumb2": (-68.495, 133.652, -0.242), 
+            "RightHandThumb3": (-70.727, 132.545, 1.556), 
+            "RightHandThumb4": (-72.412, 131.709, 2.913), 
+            "RightHandIndex1": (-71.683, 134.879, -2.495), 
+            "RightHandIndex2": (-74.972, 134.879, -2.495), 
+            "RightHandIndex3": (-77.576, 134.879, -2.495), 
+            "RightHandIndex4": (-80.181, 134.879, -2.495), 
+            "RightHandMiddle1": (-71.565, 134.682, -4.906), 
+            "RightHandMiddle2": (-75.085, 134.762, -4.906), 
+            "RightHandMiddle3": (-78.171, 134.832, -4.906), 
+            "RightHandMiddle4": (-81.569, 134.908, -4.906), 
+            "RightHandRing1": (-71.293, 134.575, -6.84), 
+            "RightHandRing2": (-74.24, 134.742, -6.84), 
+            "RightHandRing3": (-77.231, 134.912, -6.84), 
+            "RightHandRing4": (-80.134, 135.078, -6.84), 
+            "RightHandPinky1": (-70.702, 134.116, -8.847), 
+            "RightHandPinky2": (-73.811, 134.283, -8.847), 
+            "RightHandPinky3": (-75.625, 134.38, -8.847), 
+            "RightHandPinky4": (-77.461, 134.478, -8.847), 
+            "LeftUpLeg": (10.797, 91.863, -1.849), 
+            "LeftLeg": (10.797, 50.067, -0.255), 
+            "LeftFoot": (10.797, 8.223, -4.39), 
+            "LeftToeBase": (10.797, 0.001, 5.7), 
+            "LeftToe_End": (10.797, 0.0, 14.439), 
+            "RightUpLeg": (-10.797, 91.863, -1.849), 
+            "RightLeg": (-10.797, 50.066, -0.255), 
+            "RightFoot": (-10.797, 8.223, -4.39), 
+            "RightToeBase": (-10.797, 0.001, 5.7), 
+            "RightToe_End": (-10.797, 0.0, 14.439), 
+            }
         self.color_chart = {
             "gray": (0.534, 0.534, 0.534), 
             "black": (0.0, 0.0, 0.0), 
@@ -1538,8 +1604,8 @@ def extract_range_path(start: str, end: str) -> Dict[str, List]:
 
 
 
-def restore_range_path(meta_data: Dict[str, List]) -> None:
-    """ This function restores the hierarchy based on meta_data information.
+def restore_range_path(meta_data: Dict[str, List[List[str]]]) -> None:
+    f""" This function restores the hierarchy based on meta_data information.
 
     Notes
     -----
@@ -1547,7 +1613,7 @@ def restore_range_path(meta_data: Dict[str, List]) -> None:
 
     Args
     ----
-        meta_data : dict
+        meta_data : {"Hips": [["Spine", ...], ["Leg_L", ...], ["Leg_R", ... ]], ...}
 
     Examples
     --------
@@ -1555,10 +1621,12 @@ def restore_range_path(meta_data: Dict[str, List]) -> None:
     >>> orient_joint("joint2")
     >>> restore_range_path(meta_data)
     """
-    for item, parents_children in meta_data.items():
-        item_parents = parents_children[0]
-        if item_parents:
-            cmds.parent(item, item_parents)
+    for parent, children in meta_data.items():
+        for child in children:
+            if child:
+                cmds.parent(child[0], parent)
+            else:
+                continue
 
 
 
